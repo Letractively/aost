0ea1426ae9ba3c395b9bebfd3c2d43e8c5505d65
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2008-07-28 04:28:18 +0000

    refactor package name

--- trunk/rundsl.sh
+++ trunk/rundsl.sh
@@ -1,1 +1,1 @@
-java -cp ./lib/junit-3.8.2.jar:./lib/selenium-java-client-driver.jar:./lib/selenium-server.jar:./lib/groovy-all-1.5.6.jar:./dist/aost-0.3.0.jar:./out/test aost.dsl.DslScriptExecutor $1
+java -cp ./lib/junit-3.8.2.jar:./lib/selenium-java-client-driver.jar:./lib/selenium-server.jar:./lib/groovy-all-1.5.6.jar:./dist/aost-0.3.0.jar:./out/test org.aost.dsl.DslScriptExecutor $1
--- trunk/src/main/org/aost/dsl/DslScriptExecutor.groovy
+++ trunk/src/main/org/aost/dsl/DslScriptExecutor.groovy
@@ -6,7 +6,7 @@ class DslScriptExecutor {
        if(args != null && args.length == 1){
             def dsl = new File(args[0]).text
             def script = """
-                import aost.dsl.DslScriptEngine
+                import org.aost.dsl.DslScriptEngine
 
                 class DslTest extends DslScriptEngine{
                     def test(){
