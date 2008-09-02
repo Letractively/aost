ab1f8a1b8220a6b7f302146c53de1041d14653b6
Quan.Bui <Quan.Bui@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2008-09-02 02:55:15 +0000

    Create tellurium-junit-java intellij project file

---
+++ reference-projects/tellurium-junit-java/src/main/java/example/tellurium/GoogleBooksListJavaTestCase.java
@@ -0,0 +1,35 @@
+package example.tellurium;
+
+import org.tellurium.test.java.TelluriumJavaTestCase;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import example.google.NewGoogleBooksList;
+import static org.junit.Assert.*;
+
+/**
+ * Java version of Google Books List tests
+ *
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ *         Date: Aug 2, 2008
+ */
+public class GoogleBooksListJavaTestCase extends TelluriumJavaTestCase {
+    private static NewGoogleBooksList ngbl;
+
+    @BeforeClass
+    public static void initUi() {
+        ngbl = new NewGoogleBooksList();
+        ngbl.defineUi();
+    }
+
+    @Test
+    public void testBookCategory(){
+        connectUrl("http://books.google.com/");
+        String category = ngbl.getCategory();
+        assertEquals("Fiction", category);
+
+        int size = ngbl.getListSize();
+
+        assertEquals(8, size);           
+    }
+}
