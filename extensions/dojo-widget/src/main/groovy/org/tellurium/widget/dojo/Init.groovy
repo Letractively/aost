0a0e5c58ed01652a21c9564b78c75e33a3458584
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2008-09-03 03:59:13 +0000

    Initial import for the dojo-widget project

---
+++ extensions/dojo-widget/src/main/groovy/org/tellurium/widget/dojo/Init.groovy
@@ -0,0 +1,20 @@
+package org.tellurium.widget.dojo
+
+import org.tellurium.widget.WidgetBootstrap
+import org.tellurium.builder.UiObjectBuilderRegistry
+
+/**
+ * The bootstrap class for the Dojo widget module
+ * 
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: Sep 2, 2008
+ * 
+ */
+class Init implements WidgetBootstrap{
+
+    public void loadWidget(UiObjectBuilderRegistry uiObjectBuilderRegistry) {
+        
+    }
+
+}
