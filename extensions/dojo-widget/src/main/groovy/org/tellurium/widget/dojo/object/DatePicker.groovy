0ab0846347fd799fda81439f3ebdc81123dc2dba
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2008-09-04 02:43:21 +0000

    prototype dojo widget date picker

---
+++ extensions/dojo-widget/src/main/groovy/org/tellurium/widget/dojo/object/DatePicker.groovy
@@ -0,0 +1,19 @@
+package org.tellurium.widget.dojo.object
+
+import org.tellurium.widget.dojo.DojoWidget
+
+/**
+ * Tellurium Widget object for the Dojo Date Picker widget
+ *
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: Sep 3, 2008
+ * 
+ */
+class DatePicker extends DojoWidget{
+
+    public void defineWidget() {
+        
+    }
+
+}
