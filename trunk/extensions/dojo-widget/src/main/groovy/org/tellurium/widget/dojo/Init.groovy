dc2bf03401a7aefd5d181b074a148a72a247878e
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2008-09-04 04:13:19 +0000

    prototype dojo widget date picker

--- trunk/extensions/dojo-widget/src/main/groovy/org/tellurium/widget/dojo/Init.groovy
+++ trunk/extensions/dojo-widget/src/main/groovy/org/tellurium/widget/dojo/Init.groovy
@@ -2,6 +2,7 @@ package org.tellurium.widget.dojo
 
 import org.tellurium.widget.WidgetBootstrap
 import org.tellurium.builder.UiObjectBuilderRegistry
+import org.tellurium.widget.dojo.builder.DatePickerBuilder
 
 /**
  * The bootstrap class for the Dojo widget module
@@ -14,7 +15,12 @@ import org.tellurium.builder.UiObjectBuilderRegistry
 class Init implements WidgetBootstrap{
 
     public void loadWidget(UiObjectBuilderRegistry uiObjectBuilderRegistry) {
-        
+        if(uiObjectBuilderRegistry != null){
+           uiObjectBuilderRegistry.registerBuilder(getFullName("DatePicker"), new DatePickerBuilder())      
+        }
     }
 
+    protected String getFullName(String name){
+        return DojoWidget.NAMESPACE + DojoWidget.NAMESPACE_SUFFIX + name
+    }
 }
--- trunk/extensions/dojo-widget/src/main/groovy/org/tellurium/widget/dojo/object/DatePicker.groovy
+++ trunk/extensions/dojo-widget/src/main/groovy/org/tellurium/widget/dojo/object/DatePicker.groovy
@@ -13,7 +13,35 @@ import org.tellurium.widget.dojo.DojoWidget
 class DatePicker extends DojoWidget{
 
     public void defineWidget() {
-        
+        ui.Container(uid: "DatePicker", locator: "/div[@class='datePickerContainer' and child::table[@class='calendarContainer']"){
+            Container(uid: "Title", locator: "/table[@class='calendarContainer'/thead/tr/td[@class='monthWrapper']/table[@class='monthContainer']/tbody/tr/td[@class='monthLabelContainer']"){
+                Icon(uid: "increaseWeek", locator: "/span[@dojoattachpoint='increaseWeekNode']")
+                Icon(uid: "increaseMonth", locator: "/span[@dojoattachpoint='increaseMonthNode']")
+                Icon(uid: "decreaseWeek", locator: "/span[@dojoattachpoint='decreaseWeekNode']")
+                Icon(uid: "decreaseMonth", locator: "/span[@dojoattachpoint='decreaseMonthNode']")
+                TextBox(uid: "monthLabel", locator: "/span[@dojoattachpoint='monthLabelNode']")   
+            }
+            
+        }
     }
 
+    public void increaseWeek(){
+        click "DatePicker.Title.increaseWeek"
+    }
+
+    public void increaseMonth(){
+        click "DatePicker.Title.increaseMonth"
+    }
+
+    public void decreaseWeek(){
+        click "DatePicker.Title.decreaseWeek"
+    }
+
+    public void decreaseMonth(){
+        click "DatePicker.Title.decreaseMonth"
+    }
+
+    public String getMonthLabel(){
+        return getText("DatePicker.Title.monthLabel")
+    }
 }
