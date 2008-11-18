3d508f85a3d72af3a4b89fcff367fc4f7f97057c
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2008-11-18 22:37:38 +0000

    fix issue 64 for window

---
+++ trunk/core/src/test/groovy/org/tellurium/test/TWindow.groovy
@@ -0,0 +1,24 @@
+package org.tellurium.test
+
+import org.tellurium.dsl.DslContext
+
+/**
+ * 
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: Nov 17, 2008
+ * 
+ */
+class TWindow extends DslContext{
+
+    public void defineUi() {
+         ui.Window(uid: "windowName", name: "windowName"){
+             InputBox(uid: "UserName", clocator: [id: "username", type: "text"])             
+         }
+    }
+
+    public void waitForPopup(){
+        waitForPopUp "windowName", 30000
+    }
+
+}
