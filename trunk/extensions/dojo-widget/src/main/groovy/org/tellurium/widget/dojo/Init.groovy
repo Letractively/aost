f32211b28d1d4a338e3a87d60e2d2a185807f7d1
vivekmongolu <vivekmongolu@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-01-13 17:06:13 +0000

    Registered the JtvTabContainer

--- trunk/extensions/dojo-widget/src/main/groovy/org/tellurium/widget/dojo/Init.groovy
+++ trunk/extensions/dojo-widget/src/main/groovy/org/tellurium/widget/dojo/Init.groovy
@@ -3,6 +3,7 @@ package org.tellurium.widget.dojo
 import org.tellurium.widget.WidgetBootstrap
 import org.tellurium.builder.UiObjectBuilderRegistry
 import org.tellurium.widget.dojo.builder.DatePickerBuilder
+import org.tellurium.widget.dojo.object.JtvTabContainer
 
 /**
  * The bootstrap class for the Dojo widget module
@@ -17,6 +18,7 @@ class Init implements WidgetBootstrap{
     public void loadWidget(UiObjectBuilderRegistry uiObjectBuilderRegistry) {
         if(uiObjectBuilderRegistry != null){
            uiObjectBuilderRegistry.registerBuilder(getFullName("DatePicker"), new DatePickerBuilder())      
+           uiObjectBuilderRegistry.registerBuilder(getFullName("JtvTabContainer"), new JtvTabContainer())      
         }
     }
 
