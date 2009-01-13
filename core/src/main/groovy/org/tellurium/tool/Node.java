82614a682d86afc4b248a47e2d3e1359410fc3d3
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-01-13 06:31:24 +0000

    add tool to group UI objects together

---
+++ core/src/main/groovy/org/tellurium/tool/Node.java
@@ -0,0 +1,109 @@
+package org.tellurium.tool;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * A node for the tree
+ *
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ *         Date: Dec 10, 2008
+ */
+public class Node {
+    public static final String TAG = "tag";
+    
+    private String id;
+
+    //relative xpath
+    private String xpath;
+
+    private Map<String, String> attributes = new HashMap<String, String>();
+
+    private Node parent;
+
+    private LinkedList<Node> children = new LinkedList<Node>();
+
+    public void printUI(){
+        boolean hasChildren = false;
+        if(children.size() > 0)
+            hasChildren = true;
+        StringBuffer sb = new StringBuffer();
+        sb.append(Ui.getType(attributes.get(TAG), hasChildren)).append("(UID: ").append(id).append(", clocator: [:])");
+        System.out.println(sb.toString());
+        if(hasChildren){
+            for(Node node: children){
+                node.printUI();
+            }
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getXpath() {
+        return xpath;
+    }
+
+    public void setXpath(String xpath) {
+        this.xpath = xpath;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public LinkedList<Node> getChildren() {
+        return children;
+    }
+
+    public void setChildren(LinkedList<Node> children) {
+        this.children = children;
+    }
+
+    public Map getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map attributes) {
+        this.attributes = attributes;
+    }
+
+    public boolean isEmpty(){
+        if(children != null && children.size() > 0)
+            return true;
+        
+        return false;
+    }
+
+    public void addChild(Node child){
+        children.add(child);
+    }
+    
+    public void removeChild(String uid){
+        Node child = findChild(uid);
+        if(child != null){
+            children.remove(child);
+        }
+    }
+
+    public Node findChild(String uid){
+        for(Node current: children){
+            if(current.getId().equals(uid)){
+                return current;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ core/src/test/groovy/org/tellurium/test/Tree_UT.java
@@ -0,0 +1,50 @@
+package org.tellurium.test;
+
+import org.junit.Test;
+import org.tellurium.tool.Element;
+import org.tellurium.tool.Tree;
+
+/**
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ *         Date: Jan 13, 2009
+ */
+public class Tree_UT {
+    public static final String TAG = "tag";
+
+    @Test
+    public void testAddElement(){
+        Tree tree = new Tree();
+        Element e1 = new Element();
+        e1.setUid("A");
+        e1.setXpath("/html/body/table[@id='mt']");
+        e1.addAttribute(TAG, "table");
+        tree.addElement(e1);
+
+        Element e2 = new Element();
+        e2.setUid("B");
+        e2.setXpath("/html/body/table[@id='mt']/tbody/tr/th[3]");
+        e2.addAttribute(TAG, "th");
+        tree.addElement(e2);
+
+        Element e3 = new Element();
+        e3.setUid("C");
+        e3.setXpath("/html/body/table[@id='mt']/tbody/tr/th[3]/div");
+        e3.addAttribute(TAG, "div");
+        tree.addElement(e3);
+
+        Element e4 = new Element();
+        e4.setUid("D");
+        e4.setXpath("/html/body/table[@id='mt']/tbody/tr/th[3]/div/div[4]");
+        e4.addAttribute(TAG, "div");
+        tree.addElement(e4);
+
+        Element e5 = new Element();
+        e5.setUid("E");
+        e5.setXpath("/html/body/table[@id='mt']/tbody/tr/th[3]/div/div[4]/a");
+        e4.addAttribute(TAG, "a");
+        tree.addElement(e5);
+
+        tree.printUI();
+    }
+}
