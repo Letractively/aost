aadcf45cd5d7588c7d4e15d75b3d09c71da6c253
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-01-21 23:31:35 +0000

    change Selenium Client to CustomSelenium to support custom methods

--- trunk/core/src/main/groovy/org/tellurium/client/SeleniumClient.groovy
+++ trunk/core/src/main/groovy/org/tellurium/client/SeleniumClient.groovy
@@ -1,12 +1,12 @@
 package org.tellurium.client
 
-import com.thoughtworks.selenium.Selenium
+import org.tellurium.connector.CustomSelenium
 
 /**
  * User: Jian Fang (John.Jian.Fang@gmail.com)
  */
 class SeleniumClient {
 
-   def Selenium client
+   def CustomSelenium client
 
 }
