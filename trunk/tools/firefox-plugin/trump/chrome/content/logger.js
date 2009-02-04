b0e1ab9ddd3ce7c09f6144ebcc841e94ce066499
koryak <koryak@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-02-04 04:47:21 +0000

    started work on a log window. log window functional, but needs much more work

--- trunk/tools/firefox-plugin/trump/chrome/content/logger.js
+++ trunk/tools/firefox-plugin/trump/chrome/content/logger.js
@@ -6,8 +6,10 @@ var getStackTrace = function() {
 var addLineNumber = function(loggingEvent){
     var stack = getStackTrace()[6];  //assuming that this is called from the appender this 6 is what we want!
     stack = stack.substring(stack.indexOf("chrome://trump/content/") + "chrome://trump/content/".length, stack.length);
-    loggingEvent.message = "["+stack+"] " + loggingEvent.message;
-    return loggingEvent;
+    //we need to clone the event because otherwise the same event will be used in all appenders and we wont know if the line number
+    //was added to it or not, and we dont want to use an expando.
+    var clonedEvent = new Log4js.LoggingEvent(loggingEvent.categoryName, loggingEvent.level, "["+stack+"] " + loggingEvent.message, loggingEvent.exception, loggingEvent.logger);
+    return clonedEvent;
 }
 Log4js.MozillaLineNumberJSConsoleAppender = function() {
 	this.layout = new Log4js.SimpleLayout();
@@ -25,7 +27,7 @@ Log4js.MozillaLineNumberJSConsoleAppender.prototype = Log4js.extend(new Log4js.A
 	 * @see Log4js.Appender#doAppend
 	 */
 	doAppend: function(loggingEvent) {
-        addLineNumber(loggingEvent);
+        loggingEvent = addLineNumber(loggingEvent);
         try {
 			netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
 			this.scriptError.init(this.layout.format(loggingEvent), null, null, null, null, this.getFlag(loggingEvent), loggingEvent.categoryName);
@@ -63,32 +65,27 @@ Log4js.TrumpLogAppender = function() {
 };
 Log4js.TrumpLogAppender.prototype = Log4js.extend(new Log4js.Appender(), {
 	doAppend: function(loggingEvent) {
-		var now = new Date();
-		var lines = loggingEvent.message.split("\n");
-		var headTag = document.getElementsByTagName("head")[0];
 
-		for (var i = 1; i <= lines.length; i++) {
-			var value = lines[i - 1];
-			if (i == 1) {
-				value = loggingEvent.level.toString() + ": " + value;
-			} else {
-				value = "> " + value;
-			}
-
-			var metaTag = document.createElement("div");
-			metaTag.setAttribute("name", "X-log4js:" + this.currentLine);
-			metaTag.setAttribute("content", value);
-			headTag.appendChild(metaTag);
-			this.currentLine += 1;
-		}
-	},
+        if(logWindow && logWindow.document){
+            loggingEvent = addLineNumber(loggingEvent);
+            var textbox = logWindow.document.getElementById("uiModelText");
+            logText += loggingEvent.message +"\n";
+            textbox.value = logText;
+        }
+    },
 	toString: function() {
 	    return "Log4js.TrumpLogAppender";
 	}
 });
-
+var logWindow;
+var logText = "";
+var showLogWindow = function(){
+    logWindow = window.open("chrome://trump/content/trumpLogger.xul","logWindow","chrome,centerscreen,alwaysRaised=true,resizable");
+    //TODO: pass logText into window using XUL
+}
 
 //and here is the logger!
 var logger = new Log4js.getLogger("root");
 logger.setLevel(Log4js.Level.ALL);
 logger.addAppender(new Log4js.MozillaLineNumberJSConsoleAppender());
+logger.addAppender(new Log4js.TrumpLogAppender());
