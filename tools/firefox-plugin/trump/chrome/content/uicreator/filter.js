94d80528493adf8ca01eac031126082853952959
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-03-03 01:31:34 +0000

    add mouse up event

--- tools/firefox-plugin/trump/chrome/content/uicreator/filter.js
+++ tools/firefox-plugin/trump/chrome/content/uicreator/filter.js
@@ -3,7 +3,7 @@ var blackListAttributes = ["size", "maxlength", "width", "height", "style", "ali
 //var whiteListAttributes = ["id", "name", "value", "tag", "type", "class", "action", "method", "title", "text", "href", "src", "position"]
 var whiteListAttributes = ["id", "name", "value", "tag", "type", "class", "action", "method", "title", "text", "href", "src"]
 
-var eventListAttributes = ["onclick", "ondoubleclick", "onkeyup", "onkeydown", "onkeypress", "onfocus", "onblur", "onmousedown","onmouseup", "onmouseover", "onmouseout", "onchange", "onsubmit", "onselect"]
+var eventListAttributes = ["onclick", "ondoubleclick", "onkeyup", "onkeydown", "onkeypress", "onfocus", "onblur", "onmousedown", "onmouseup", "onmouseover", "onmouseout", "onchange", "onsubmit", "onselect"]
 
 //filter out do not wanted Node attributes, most copied from builder.js
 function Filter(){
