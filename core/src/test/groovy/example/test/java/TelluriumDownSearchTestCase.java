2c1f5b2c423bb28acb4778bf95fff144eb775354
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-04-10 04:05:03 +0000

    add new test case for jQuery selector

---
+++ core/src/test/groovy/example/test/java/TelluriumDownSearchTestCase.java
@@ -0,0 +1,33 @@
+package example.test.java;
+
+import org.tellurium.test.java.TelluriumJavaTestCase;
+import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import example.other.TelluriumDownSearchModule;
+
+public class TelluriumDownSearchTestCase extends TelluriumJavaTestCase {
+    private static TelluriumDownSearchModule app;
+
+    @BeforeClass
+    public static void initUi() {
+        app = new TelluriumDownSearchModule();
+        app.defineUi();
+    }
+
+    @Before
+    public void setUpForTest() {
+        connectUrl("http://code.google.com/p/aost/downloads/list");
+    }
+
+    @Test
+    public void testTelluriumProjectPage() {
+        String[] allTypes = app.getAllDownloadTypes();
+        assertNotNull(allTypes);
+        assertTrue(allTypes[1].contains("All downloads"));
+        app.selectDownloadType(allTypes[1]);
+        app.searchDownload("TrUMP");
+    }
+}
