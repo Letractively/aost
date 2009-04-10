38439f957ba9bf78f1089d402c06943035154bbf
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-04-10 04:05:03 +0000

    add new test case for jQuery selector

---
+++ trunk/core/src/test/groovy/example/other/TelluriumDownSearchModule.groovy
@@ -0,0 +1,34 @@
+package example.other
+
+import org.tellurium.dsl.DslContext
+
+
+public class TelluriumDownSearchModule extends DslContext {
+
+  public void defineUi() {
+    
+    useJQuerySelector()
+    
+//    ui.Form(uid: "TelluriumDownload", clocator: [tag: "form", method: "get", action: "list"], group: "true") {
+    ui.Form(uid: "TelluriumDownload", clocator: [tag: "form", method: "get", action: "list"]) {
+      Selector(uid: "DownloadType", clocator: [tag: "select", name: "can", id: "can"])
+      InputBox(uid: "Input", clocator: [tag: "input", type: "text", name: "q", id: "q"])
+      SubmitButton(uid: "Search", clocator: [tag: "input", type: "submit", value: "Search"])
+    }
+  }
+
+  //Add your methods here
+  public void searchDownload(String keyword) {
+    keyType "TelluriumDownload.Input", keyword
+    click "TelluriumDownload.Search"
+    waitForPageToLoad 30000
+  }
+
+  public String[] getAllDownloadTypes() {
+    return getSelectOptions("TelluriumDownload.DownloadType")
+  }
+
+  public void selectDownloadType(String type) {
+    selectByLabel "TelluriumDownload.DownloadType", type
+  }
+}
