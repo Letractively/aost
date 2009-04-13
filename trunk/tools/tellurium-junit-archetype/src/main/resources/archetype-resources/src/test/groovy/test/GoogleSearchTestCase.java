9608fe3c6d6222a6d877ce36a0fdc337be3f254f
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-04-13 20:25:00 +0000

    add jQuery example to tellurium-junit-archetype

--- trunk/tools/tellurium-junit-archetype/src/main/resources/archetype-resources/src/test/groovy/test/GoogleSearchTestCase.java
+++ trunk/tools/tellurium-junit-archetype/src/main/resources/archetype-resources/src/test/groovy/test/GoogleSearchTestCase.java
@@ -27,12 +27,30 @@ public class GoogleSearchTestCase extends TelluriumJavaTestCase {
     }
 
     @Test
-    public void testGoogleSearch() {
+    public void testGoogleSearchWithXPath() {
+        //turn off jQuery Selector
+        gsm.disableJQuerySelector();
         gsm.doGoogleSearch("tellurium Groovy Test");
     }
 
     @Test
-    public void testGoogleSearchFeelingLucky() {
+    public void testGoogleSearchFeelingLuckyWithXPath() {
+        //turn off jQuery Selector
+        gsm.disableJQuerySelector();
+        gsm.doImFeelingLucky("tellurium automated Testing");
+    }
+
+    @Test
+    public void testGoogleSearchWithSelector() {
+        //turn on jQuery Selector
+        gsm.useJQuerySelector();
+        gsm.doGoogleSearch("tellurium Groovy Test");
+    }
+
+    @Test
+    public void testGoogleSearchFeelingLuckyWithSelector() {
+        //turn on jQuery Selector
+        gsm.useJQuerySelector();
         gsm.doImFeelingLucky("tellurium automated Testing");
     }
 }
