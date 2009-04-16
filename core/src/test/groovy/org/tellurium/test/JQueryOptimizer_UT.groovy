2679ffaea8201a821dcb109eae6efe622cba3329
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-04-16 05:33:14 +0000

    add jQuery optimizer

---
+++ core/src/test/groovy/org/tellurium/test/JQueryOptimizer_UT.groovy
@@ -0,0 +1,31 @@
+package org.tellurium.test
+
+import org.tellurium.locator.JQueryOptimizer
+
+/**
+ * 
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: Apr 15, 2009
+ * 
+ */
+
+public class JQueryOptimizer_UT extends GroovyTestCase{
+  protected JQueryOptimizer jqp = new JQueryOptimizer()
+
+  public void testRemovePrefix(){
+    String jqs = "jquery=table#resultstable > tbody > tr > td"
+    String result = jqp.removePrefix(jqs)
+    assertEquals("table#resultstable > tbody > tr > td", result)
+    jqs = " table#resultstable > tbody > tr > td"
+    result = jqp.removePrefix(jqs)
+    assertEquals("table#resultstable > tbody > tr > td", result)
+  }
+
+  public void testPickIdSelector(){
+    String jqs = "form[method=get]:has(select#can, span:contains(for), input[type=text][name=q], input[value=Search][type=submit]) select#can"
+    String result = jqp.pickIdSelector(jqs)
+    assertNotNull(result)
+    assertEquals("select#can", result)
+  }
+}
