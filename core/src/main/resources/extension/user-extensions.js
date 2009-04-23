c22d1417df84b8aa2b5f2a82a672b7308ab3ad83
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-04-23 16:23:32 +0000

    yield the user-extensions.js to users and we will not use it

--- core/src/main/resources/extension/user-extensions.js
+++ core/src/main/resources/extension/user-extensions.js
@@ -1,88 +1,17 @@
 // User extensions can be added here.
+// All do* methods on the Selenium prototype are added as actions.
+// Eg add a typeRepeated action to Selenium, which types the text twice into a text box.
+// The typeTwiceAndWait command will be available automatically
 
-//add custom jQuery Selector :te_text()
-//
+/*
+Selenium.prototype.doTypeRepeated = function(locator, text) {
+    // All locator-strategies are automatically handled by "findElement"
+    var element = this.page().findElement(locator);
 
-$.extend($.expr[':'],{
-  te_text: function(a,i,m) {
-            return $(a).text() === m[3];
-        }
-});
+    // Create the text to type
+    var valueToType = text + text;
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-Selenium.prototype.getSelectorProperties = function(jq, p){
-	var p =  eval('(' + p + ')'); //eval json
-	var e = this.browserbot.findElement("jqueryall="+jq);
-	var out = [];
-	for(var i = 0; i < e.length; i++){
-		var stuff = {};
-		for(var j = 0; j < p.length; j++){
-			var prop = null;
-			if(typeof e[i][p[j]] !== "undefined"){
-				prop = e[i][p[j]];
-			}
-			stuff[p[j]] = prop;	//gather the requested stuff
-		}
-		out.push(stuff);
-	}
-	return JSON.stringify(out);
+    // Replace the element text with the new text
+    this.page().replaceText(element, valueToType);
 };
-
-Selenium.prototype.getSelectorText = function(jq){
-	var e = this.browserbot.findElement("jqueryall="+jq);
-	var out = [];
-	for(var i = 0; i < e.length; i++){
-		out.push($(e[i]).text());
-	}
-	return JSON.stringify(out);
-};
-
-//generic function call. best hope the function returns json, or we are going to be sad
-Selenium.prototype.getSelectorFunctionCall = function(jq, fn){
-	var e = this.browserbot.findElement("jqueryall="+jq);
-	fn = eval('(' + eval('(' +fn+')')[0]+')');
-	return JSON.stringify(fn.apply(e));
-};
-
-Selenium.prototype.getAllText = function(locator){
-	var $e = $(this.browserbot.findElement(locator));
-	var out = [];
-	$e.each(function(){
-		out.push($(this).text());
-	});
-	return JSON.stringify(out);
-};
-
-Selenium.prototype.getJQuerySelectorCount = function(locator){
-	var $e = $(this.browserbot.findElement(locator));
-    if($e == null)
-        return 0;
-
-	return $e.length;
-};
-
-Selenium.prototype.getCSS = function(locator, cssName){
-	var $e = $(this.browserbot.findElement(locator));
-	var out = [];
-	$e.each(function(){
-		out.push($(this).css(cssName));
-	});
-	return JSON.stringify(out);
-};
-
-Selenium.prototype.isDisabled = function(locator){
-    var $e = $(this.browserbot.findElement(locator));
-    if($e == null || $e.length < 1)
-        Assert.fail("Cannot find Element for " + locator);
-    if($e.length > 1)
-        Assert.fail("Element for " + locator + " is not unique.");
-    return $e.attr('disabled') == true
-};
+*/
