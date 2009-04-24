5914d6eca30558cc9c33cb4039bad57df8b41e58
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-04-24 15:15:08 +0000

    add user-extensions.js

--- engine/src/main/resources/core/scripts/tellurium-extensions.js
+++ engine/src/main/resources/core/scripts/tellurium-extensions.js
@@ -121,16 +121,3 @@ Selenium.prototype.getCacheMaxSize = function(){
 Selenium.prototype.getCacheUsage = function(){
     return tellurium.getCacheUsage();
 };
-
-/*
-Selenium.prototype.doTypeRepeated = function(locator, text) {
-    // All locator-strategies are automatically handled by "findElement"
-    var element = this.page().findElement(locator);
-
-    // Create the text to type
-    var valueToType = text + text;
-
-    // Replace the element text with the new text
-    this.page().replaceText(element, valueToType);
-};
-*/
