ae520b12217da89087f463c3c2b25e32a2c07437
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-04-27 17:20:24 +0000

    add original jQuery selector back for no cache to remove overhead

--- trunk/engine/src/main/resources/core/scripts/tellurium.js
+++ trunk/engine/src/main/resources/core/scripts/tellurium.js
@@ -202,7 +202,35 @@ Tellurium.prototype.addSelectorToCache = function(key, data){
     }
 };
 
-Tellurium.prototype.locateElementByJQuery = function(locator, inDocument, inWindow){
+Tellurium.prototype.locateElementByJQuerySelector = function(locator, inDocument, inWindow){
+    var loc = locator;
+    var attr = null;
+    var isattr = false;
+    var inx = locator.lastIndexOf('@');
+    if (inx != -1) {
+        loc = locator.substring(0, inx);
+        attr = locator.substring(inx + 1);
+        isattr = true;
+    }
+    var found = jQuery(inDocument).find(loc);
+    if (found.length == 1) {
+        if (isattr) {
+            return found[0].getAttributeNode(attr);
+        } else {
+            return found[0];
+        }
+    } else if (found.length > 1) {
+        if (isattr) {
+            return found.get().getAttributeNode(attr);
+        } else {
+            return found.get();
+        }
+    } else {
+        return null;
+    }
+};
+
+Tellurium.prototype.locateElementByCacheAwareJQuerySelector = function(locator, inDocument, inWindow){
 
     if(inWindow != this.currentWindow){
 //        jslogger.debug("Bind cleaning cache to window unload envent " + inWindow);
