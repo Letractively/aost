b094cebc408938788197c9f0797d04d2236ea7d0
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-05-07 04:36:06 +0000

    Fix IncludeUIModule_UT

--- trunk/core/src/test/groovy/org/tellurium/test/IncludeUiModule_UT.groovy
+++ trunk/core/src/test/groovy/org/tellurium/test/IncludeUiModule_UT.groovy
@@ -9,19 +9,19 @@ public class IncludeUiModule_UT extends GroovyTestCase {
     module.defineUi()
     
     String result = module.getXPath("SearchModule.Input")
-    assertEquals("/descendant-or-self::td[descendant::input[@title=\"Google Search\"] and descendant::input[@name=\"btnG\" and @value=\"Google Search\" and @type=\"submit\"] and descendant::input[@value=\"I'm Feeling Lucky\" and @type=\"submit\"]]/descendant-or-self::input[@title=\"Google Search\"]", result)
+    assertEquals("//descendant-or-self::td[descendant::input[@title=\"Google Search\"] and descendant::input[@name=\"btnG\" and @value=\"Google Search\" and @type=\"submit\"] and descendant::input[@value=\"I'm Feeling Lucky\" and @type=\"submit\"]]/descendant-or-self::input[@title=\"Google Search\"]", result)
 
     result = module.getXPath("Google.SearchModule.Input")
-    assertEquals("/descendant-or-self::table/descendant-or-self::td[descendant::input[@title=\"Google Search\"] and descendant::input[@name=\"btnG\" and @value=\"Google Search\" and @type=\"submit\"] and descendant::input[@value=\"I'm Feeling Lucky\" and @type=\"submit\"]]/descendant-or-self::input[@title=\"Google Search\"]", result)
+    assertEquals("//descendant-or-self::table/descendant-or-self::td[descendant::input[@title=\"Google Search\"] and descendant::input[@name=\"btnG\" and @value=\"Google Search\" and @type=\"submit\"] and descendant::input[@value=\"I'm Feeling Lucky\" and @type=\"submit\"]]/descendant-or-self::input[@title=\"Google Search\"]", result)
 
     result = module.getXPath("GoogleBooksList.category")
-    assertEquals("/descendant-or-self::table[descendant::div[@class=\"sub_cat_title\"] and descendant::div[@class=\"sub_cat_section\"] and @id=\"hp_table\"]/descendant-or-self::div[@class=\"sub_cat_title\"]", result)
+    assertEquals("//descendant-or-self::table[descendant::div[@class=\"sub_cat_title\"] and descendant::div[@class=\"sub_cat_section\"] and @id=\"hp_table\"]/descendant-or-self::div[@class=\"sub_cat_title\"]", result)
 
     result = module.getXPath("Test.newcategory")
-    assertEquals("/descendant-or-self::div/descendant-or-self::div[@class=\"sub_cat_title\"]", result)
+    assertEquals("//descendant-or-self::div/descendant-or-self::div[@class=\"sub_cat_title\"]", result)
 
     result = module.getXPath("Test.newsubcategory")
-    assertEquals("/descendant-or-self::div/descendant-or-self::div[@class=\"sub_cat_section\"]", result)
+    assertEquals("//descendant-or-self::div/descendant-or-self::div[@class=\"sub_cat_section\"]", result)
   }
 
   public void testDump(){
