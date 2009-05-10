8e498ba393235c1aa49b5cf5a8a9d60932ade38c
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-05-10 05:32:47 +0000

    update rundsl scripts

--- core/rundsl.sh
+++ core/rundsl.sh
@@ -1,1 +1,1 @@
-java -cp ./lib/selenium-java-client-driver.jar:./lib/selenium-server.jar:./lib/groovy-all-1.5.7.jar:./lib/junit-4.4.jar:./dist/tellurium-0.6.0.jar:./out/test org.tellurium.dsl.DslScriptExecutor $1
+java -cp ./lib/selenium-java-client-driver.jar:./lib/selenium-server.jar:./lib/groovy-all-1.6.0.jar:./lib/json_simple-r1.jar:./lib/stringtree-json-2.0.10.jar:./lib/junit-4.4.jar:./dist/tellurium-0.6.0.jar:./out/test org.tellurium.dsl.DslScriptExecutor $1
