bc2bb51be6efcd3f2c1120a7aa1ed1922cba2e1b
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-05-11 03:56:45 +0000

    re-cut 0.6.0 RC01 branch

---
+++ branches/tellurium-0.6.0-RC01/core/src/main/groovy/org/tellurium/test/crosscut/Logger.groovy
@@ -0,0 +1,16 @@
+package org.tellurium.test.crosscut
+
+/**
+ * 
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: Apr 27, 2009
+ * 
+ */
+public interface Logger {
+
+  public void log(String message)
+
+  public void addAppender(Appender appender)
+  
+}
---
+++ branches/tellurium-0.6.0-RC01/core/src/test/groovy/org/tellurium/test/Data.groovy
@@ -0,0 +1,13 @@
+package org.tellurium.test
+/**
+ * 
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: May 1, 2009
+ * 
+ */
+
+public class Data {
+
+  int key;
+}
