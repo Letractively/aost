aee55ee6a6491180423c6167b3a9cc7f95c1323f
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-05-18 13:45:10 +0000

    add dojoAttachEvent processing

--- tools/firefox-plugin/trump/chrome/content/uicreator/filter.js
+++ tools/firefox-plugin/trump/chrome/content/uicreator/filter.js
@@ -1,12 +1,16 @@
-var blackListAttributes = ["size", "maxlength", "width", "height", "style", "align", "autocomplete", "nowrap"]
+var blackListAttributes = ["size", "maxlength", "width", "height", "style", "align", "autocomplete", "nowrap"];
 
 //var whiteListAttributes = ["id", "name", "value", "tag", "type", "class", "action", "method", "title", "text", "href", "src", "position"]
-var whiteListAttributes = ["id", "name", "value", "tag", "type", "class", "action", "method", "title", "text", "href", "src"]
+var whiteListAttributes = ["id", "name", "value", "tag", "type", "class", "action", "method", "title", "text", "href", "src"];
 
-var eventListAttributes = ["onclick", "ondblclick", "onkeyup", "onkeydown", "onkeypress", "onfocus", "onblur", "onmousedown", "onmouseup", "onmouseover", "onmouseout", "onchange", "onsubmit", "onselect"]
+var eventListAttributes = ["onclick", "ondblclick", "onkeyup", "onkeydown", "onkeypress", "onfocus", "onblur", "onmousedown", "onmouseup", "onmouseover", "onmouseout", "onchange", "onsubmit", "onselect"];
 
 //filter out do not wanted Node attributes, most copied from builder.js
 function Filter(){
+    this.SEPARATOR = ",";
+    this.ATTR_SEPARATOR = ":";
+    this.DOJO_ATTACH_EVENT = "dojoAttachEvent";
+
     this.eventmap =  new HashMap();
     this.eventmap.set("onclick", "click");
     this.eventmap.set("ondblclick", "doubleclick");
@@ -19,7 +23,11 @@ function Filter(){
     this.eventmap.set("onmouseup", "mouseUp");
     this.eventmap.set("onmouseover", "mouseOver");
     this.eventmap.set("onmouseout", "mouseOut");
-}
+
+    //for dojoAttachEvent
+    this.eventmap.set("onmouseenter", "mouseOver");
+    this.eventmap.set("onmouseleave", "mouseOut");
+};
  
 /**
  *  Iterates thru the attributes of a node
@@ -31,23 +39,40 @@ Filter.prototype.getAttributesString = function(node){
     var attr="";
     for(var i=0; i < attributes.length; ++i){
         if(i != 0){
-            attr+=", "
+            attr+=", ";
         }
         attr+= attributes[i].name + ": \""+ attributes[i].value+"\"";
     }
     return attr;
-}
+};
 
 Filter.prototype.getNotBlackListedAttributes = function(attributes){
     var wantedAttributes = new HashMap();
     for(var i=0; i < attributes.length; ++i){
         if(this.isNotBlackListed(attributes[i].name)){
-            wantedAttributes.set(attributes[i].name, attributes[i].value);
+            if(this.DOJO_ATTACH_EVENT == attributes[i].name){
+                this.processDojoAttachEventAttribute(attributes[i].value, wantedAttributes);
+            }else{
+                wantedAttributes.set(attributes[i].name, attributes[i].value);
+            }
         }
     }
 
     return wantedAttributes;
-}
+};
+
+Filter.prototype.processDojoAttachEventAttribute = function(attrValue, map){
+    if(trimString(attrValue).length > 0 ){
+        var splited = attrValue.split(this.SEPARATOR);
+        for(var i=0; i<splited.length; i++){
+            if(trimString(splited[i]).length > 0){
+                var pair = trimString(splited[i]).split(this.ATTR_SEPARATOR);
+                map.set(trimString(pair[0]), trimString(pair[1]));
+            }
+        }
+
+    }      
+};
 
 Filter.prototype.getWhiteListedAttributes = function(attributes){
     var wantedAttributes = new HashMap();
@@ -62,24 +87,24 @@ Filter.prototype.getWhiteListedAttributes = function(attributes){
     }
 
     return wantedAttributes;
-}
+};
 
 Filter.prototype.isNotBlackListed = function(attribute){
     return blackListAttributes.indexOf(attribute) == -1;
-}
+};
 
 Filter.prototype.isWhiteListed = function(attribute){
     return whiteListAttributes.indexOf(attribute) != -1;
-}
+};
 
 Filter.prototype.isEventAttribute = function(attribute){
     return eventListAttributes.indexOf(attribute) != -1;
-}
+};
 
 Filter.prototype.isEvent = function(attribute){
     var value = this.eventmap.get(attribute);
     return value != null;
-}
+};
 
 Filter.prototype.processEventAttributes = function(attributes){
     var respond = new Array();
@@ -98,4 +123,4 @@ Filter.prototype.processEventAttributes = function(attributes){
     }
 
     return respond;
-}
+};
