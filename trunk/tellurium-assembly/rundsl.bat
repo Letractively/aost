358b93a81ef60364f96356bfef2fae723fef4144
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-05-19 01:44:15 +0000

    update assembly project

--- trunk/tellurium-assembly/rundsl.bat
+++ trunk/tellurium-assembly/rundsl.bat
@@ -1,4 +1,4 @@
 @echo off
 SET TELLURIUM_HOME=.
-SET TELLURIUM_CLASSPATH=%TELLURIUM_HOME%\lib\selenium-java-client-driver.jar;%TELLURIUM_HOME%\lib\selenium-server.jar;%TELLURIUM_HOME%\lib\groovy-all-1.5.6.jar;%TELLURIUM_HOME%\dist\tellurium-0.5.0.jar;%TELLURIUM_HOME%\out\test\;%TELLURIUM_HOME%\out\production\
+SET TELLURIUM_CLASSPATH=%TELLURIUM_HOME%\lib\selenium-java-client-driver.jar;%TELLURIUM_HOME%\lib\selenium-server.jar;%TELLURIUM_HOME%\lib\groovy-all-1.6.0.jar;%TELLURIUM_HOME%\dist\tellurium-0.6.0.jar;%TELLURIUM_HOME%\out\test\;%TELLURIUM_HOME%\out\production\
 java -cp %TELLURIUM_CLASSPATH% org.tellurium.dsl.DslScriptExecutor %1
