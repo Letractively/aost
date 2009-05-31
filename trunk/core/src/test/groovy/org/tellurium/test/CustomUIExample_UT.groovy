558a8306ad129fa85a51ec3b01e38429c659f1f3
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-05-31 04:10:22 +0000

    add user's UI modules

---
+++ trunk/core/src/test/groovy/org/tellurium/test/CustomUIExample_UT.groovy
@@ -0,0 +1,30 @@
+package org.tellurium.test
+/**
+ * 
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: May 27, 2009
+ * 
+ */
+
+public class CustomUIExample_UT extends GroovyTestCase {
+  public void testDump(){
+    CustomUIExample cue = new CustomUIExample();
+    cue.defineUi();
+    cue.dump("switch");
+  }
+
+  public void testCustomUICall(){
+    CustomUIExample cue = new CustomUIExample();
+    cue.defineUi();
+    int tabnum = 3;
+    String status = cue.getXCaseStatus(tabnum); 
+  }
+
+  public void testGroup(){
+    CustomUIExample cue = new CustomUIExample();
+    cue.defineUi();
+    cue.dump("caseRecordPopUp");    
+  }
+
+}
