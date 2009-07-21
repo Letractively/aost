0132cbd193441b7847462f0ea8322bc934b66d79
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-07-21 05:08:37 +0000

    add mock http server

--- trunk/core/src/test/groovy/example/test/java/ListTestCase.java
+++ trunk/core/src/test/groovy/example/test/java/ListTestCase.java
@@ -1,9 +1,12 @@
 package example.test.java;
 
 import example.other.ListModule;
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.tellurium.test.java.TelluriumJavaTestCase;
+import org.tellurium.test.mock.MockHttpHandler;
+import org.tellurium.test.mock.MockHttpServer;
 
 /**
  * @author Jian Fang (John.Jian.Fang@gmail.com)
@@ -11,8 +14,16 @@ import org.tellurium.test.java.TelluriumJavaTestCase;
  *         Date: Jul 20, 2009
  */
 public class ListTestCase  extends TelluriumJavaTestCase {
+    private static MockHttpServer server;
 
-    @Ignore
+    @BeforeClass
+    public static void setUp(){
+        MockHttpHandler handler = new MockHttpHandler();
+        handler.setBody(ListModule.LIST_BODY);
+        server = new MockHttpServer();
+        server.start(8080, "/list.html", handler);
+    }
+
     @Test
     public void testGetSeparatorAttribute(){
         ListModule lm = new ListModule();
@@ -24,5 +35,10 @@ public class ListTestCase  extends TelluriumJavaTestCase {
         lm.useJQuerySelector();
         attr = (String)lm.getParentAttribute("rotator.tnails[6]", "class");
         System.out.println("jQuery: Class attribute for rotator.tnails[6] " + attr);
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        server.stop();    
     }
 }
