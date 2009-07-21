f0cf3c6951e078b5836169166674bb43273fde70
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-07-21 18:13:39 +0000

    modify the HTML source from user

--- core/src/test/groovy/example/other/ListModule.groovy
+++ core/src/test/groovy/example/other/ListModule.groovy
@@ -15,26 +15,26 @@ public class ListModule extends DslContext {
 <div class="thumbnails">
     <ul>
         <li class="thumbnail">
-            <img alt="Dan Henderson Ruins UFC's Hopes For UFC 105 and Michael Bisping"
+            <img alt="Image 1"
                  src="/images_root/image_pictures/0518/0180/127482_crop_85x60.jpg"/>
         </li>
         <li class="thumbnail">
-            <img alt="Underdog Sports: Best Pro Women Athletes Today No. 10-6"
+            <img alt="Image 2"
                  src="/images_root/image_pictures/0517/9892/98173_crop_85x60.jpg"/>
         </li>
         <li class="thumbnail">
-            <img alt="The Chicago Cubs: My First MLB Game Pt.2; The Magic Of Hatfeild Hotdogs!"
+            <img alt="Image 3"
                  src="/images_root/image_pictures/0517/9210/100722_crop_85x60.jpg"/>
         </li>
         <li class="thumbnail">
         </li>
         <li class="thumbnail active">
-            <img alt="Boston Red Sox Weekly Recap (Week 15)"
+            <img alt="Image 4"
                  src="/images_root/image_pictures/0518/0204/6839_crop_85x60.jpg"/>
         </li>
         <li class="thumbnail potd">
             <div class="potd-icon png-fix"/>
-            <img alt="Browns Fans Want To See More Of Jerome Harrison, You're Getting Your Wish"
+            <img alt="Image 5"
                  src="/images_root/image_pictures/0517/9916/111034_crop_85x60.jpg"/>
         </li>
     </ul>
