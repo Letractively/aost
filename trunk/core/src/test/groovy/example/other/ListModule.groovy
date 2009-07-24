bd3615b317ec88a65274c6f3b4936d6a1a0cf3f5
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-07-24 03:32:06 +0000

    add custom UI module

--- trunk/core/src/test/groovy/example/other/ListModule.groovy
+++ trunk/core/src/test/groovy/example/other/ListModule.groovy
@@ -16,26 +16,26 @@ public class ListModule extends DslContext {
     <ul>
         <li class="thumbnail">
             <img alt="Image 1"
-                 src="/images_root/image_pictures/0518/0180/127482_crop_85x60.jpg"/>
+                 src="/images_root/image_pictures/01.jpg"/>
         </li>
         <li class="thumbnail">
             <img alt="Image 2"
-                 src="/images_root/image_pictures/0517/9892/98173_crop_85x60.jpg"/>
+                 src="/images_root/image_pictures/02.jpg"/>
         </li>
         <li class="thumbnail">
             <img alt="Image 3"
-                 src="/images_root/image_pictures/0517/9210/100722_crop_85x60.jpg"/>
+                 src="/images_root/image_pictures/030.jpg"/>
         </li>
         <li class="thumbnail">
         </li>
         <li class="thumbnail active">
             <img alt="Image 4"
-                 src="/images_root/image_pictures/0518/0204/6839_crop_85x60.jpg"/>
+                 src="/images_root/image_pictures/04.jpg"/>
         </li>
         <li class="thumbnail potd">
             <div class="potd-icon png-fix"/>
             <img alt="Image 5"
-                 src="/images_root/image_pictures/0517/9916/111034_crop_85x60.jpg"/>
+                 src="/images_root/image_pictures/05.jpg"/>
         </li>
     </ul>
 </div>    
