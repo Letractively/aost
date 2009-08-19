17ab2ff568442d5a348959e0b1a4b57747512999
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-08-19 20:12:13 +0000

    add closest to Engine in the diagnose method

--- engine/src/main/resources/core/scripts/utils.js
+++ engine/src/main/resources/core/scripts/utils.js
@@ -290,10 +290,10 @@ String.prototype.trim = function(){
     return (this.replace(/^[\s\xA0]+/, "").replace(/[\s\xA0]+$/, ""));
 };
 
-String.prototype.startsWith = function(str){
-    return (this.match("^"+str)==str);
-};
+//String.prototype.startsWith = function(str){
+//    return (this.match("^"+str)==str);
+//};
 
-String.prototype.endsWith = function(str){
-    return (this.match(str+"$")==str);
-};
+//String.prototype.endsWith = function(str){
+//    return (this.match(str+"$")==str);
+//};
