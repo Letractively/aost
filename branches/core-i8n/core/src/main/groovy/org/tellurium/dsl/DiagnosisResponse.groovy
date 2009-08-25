d382791907836c6897e44593b3c4511b37c7e8d7
ajay.ravichandran@gmail.com <ajay.ravichandran@gmail.com@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-08-25 04:20:53 +0000

    Internationalization prototype for core

---
+++ branches/core-i8n/core/src/main/groovy/org/tellurium/dsl/DiagnosisResponse.groovy
@@ -0,0 +1,84 @@
+package org.tellurium.dsl
+/**
+ *
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: Aug 17, 2009
+ *
+ */
+
+public class DiagnosisResponse {
+  public static final String UID = "uid"
+  private String uid;
+
+  public static final String COUNT = "count"
+  private int count;
+
+  public static final String MATCHES = "matches"
+  private ArrayList<String> matches;
+
+  public static final String PARENTS = "parents"
+  private ArrayList<String> parents;
+
+  public static final String CLOSEST = "closest"
+  private ArrayList<String> closest;
+
+  private static final String HTML = "html"
+  private String html;
+
+  def DiagnosisResponse() {
+  }
+
+  def DiagnosisResponse(Map map) {
+    this.uid = map.get(UID);
+    this.count = map.get(COUNT);
+    this.matches = map.get(MATCHES);
+    this.parents = map.get(PARENTS);
+    this.closest = map.get(CLOSEST);
+    this.html = map.get(HTML);
+  }
+
+  public void show() {
+    println("\nDiagnosis Result for " + uid);
+    println("\n-------------------------------------------------------\n")
+
+    println("\tMatching count: " + count + "\n");
+    if(matches != null && matches.size() > 0){
+      println("\tMatch elements: \n");
+      int i = 0;
+      matches.each {String elem ->
+        i++;
+        println("\t--- Element " + i + " ---\n");
+        println(elem + "\n");
+      }
+    }
+
+    if(parents != null && parents.size() > 0){
+      println("\n\tParents: \n");
+      int j = 0;
+      parents.each {String elem ->
+        j++;
+        println("\t--- Parent " + j + "---\n");
+        println(elem + "\n");
+      }
+    }
+
+    if(closest != null && closest.size() > 0){
+      println("\n\tClosest: \n");
+      int k = 0;
+
+      closest.each {String elem ->
+        k++;
+        println("\t--- closest element " + k + "---\n");
+        println(elem + "\n");
+      }
+    }
+
+    if(html != null){
+      println("HTML Source: \n")
+      println(html);
+      println("\n");
+    }
+    println("\n-------------------------------------------------------\n")
+  }
+}
