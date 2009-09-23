2971b3c75a0735452ca5afdd660a23f2da51e9c7
matt.senter <matt.senter@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-09-23 18:38:13 +0000

    created tellurium-examples project

---
+++ trunk/examples/src/main/groovy/org/tellurium/example/other/TelluriumIssueModule.groovy
@@ -0,0 +1,121 @@
+package org.tellurium.example.other
+
+import org.tellurium.dsl.DslContext
+import org.tellurium.object.UiObject
+
+public class TelluriumIssueModule extends DslContext {
+
+  public void defineUi() {
+
+    //define UI module of a form include issue type selector and issue search
+    ui.Form(uid: "issueSearch", clocator: [action: "list", method: "get"], group: "true") {
+      Selector(uid: "issueType", clocator: [name: "can", id: "can"])
+      TextBox(uid: "searchLabel", clocator: [tag: "span", text: "*for"])
+      InputBox(uid: "searchBox", clocator: [type: "text", name: "q"])
+      SubmitButton(uid: "searchButton", clocator: [value: "Search"])
+    }
+
+    ui.Table(uid: "issueResult", clocator: [id: "resultstable", class: "results"], group: "true") {
+      TextBox(uid: "header: 1", clocator: [:])
+      UrlLink(uid: "header: 2", clocator: [text: "*ID"])
+      UrlLink(uid: "header: 3", clocator: [text: "*Type"])
+      UrlLink(uid: "header: 4", clocator: [text: "*Status"])
+      UrlLink(uid: "header: 5", clocator: [text: "*Priority"])
+      UrlLink(uid: "header: 6", clocator: [text: "*Milestone"])
+      UrlLink(uid: "header: 7", clocator: [text: "*Owner"])
+      UrlLink(uid: "header: 9", clocator: [text: "*Summary + Labels"])
+      UrlLink(uid: "header: 10", clocator: [text: "*..."])
+
+      //define table elements
+      //for the border column
+      TextBox(uid: "row: *, column: 1", clocator: [:])
+      TextBox(uid: "row: *, column: 8", clocator: [:])
+      TextBox(uid: "row: *, column: 10", clocator: [:])
+      //For the rest, just UrlLink
+      UrlLink(uid: "all", clocator: [:])
+    }
+
+    ui.Table(uid: "issueResultWithCache", cacheable: "true", noCacheForChildren: "false", clocator: [id: "resultstable", class: "results"], group: "true") {
+      //define table header
+      //for the border column
+      TextBox(uid: "header: 1", clocator: [:])
+      UrlLink(uid: "header: 2", clocator: [text: "*ID"])
+      UrlLink(uid: "header: 3", clocator: [text: "*Type"])
+      UrlLink(uid: "header: 4", clocator: [text: "*Status"])
+      UrlLink(uid: "header: 5", clocator: [text: "*Priority"])
+      UrlLink(uid: "header: 6", clocator: [text: "*Milestone"])
+      UrlLink(uid: "header: 7", clocator: [text: "*Owner"])
+      UrlLink(uid: "header: 9", clocator: [text: "*Summary + Labels"])
+      UrlLink(uid: "header: 10", clocator: [text: "*..."])
+
+      //define table elements
+      //for the border column
+      TextBox(uid: "row: *, column: 1", clocator: [:])
+      TextBox(uid: "row: *, column: 8", clocator: [:])
+      TextBox(uid: "row: *, column: 10", clocator: [:])
+      //For the rest, just UrlLink
+      UrlLink(uid: "all", clocator: [:])
+    }
+
+  }
+
+  public List<String> getDataForColumn(int column){
+        int nrow = getTableMaxRowNum("issueResult")
+        if(nrow > 20) nrow = 20
+        List<String> lst = new ArrayList<String>()
+        for(int i=1; i<nrow; i++){
+            lst.add(getText("issueResult[${i}][${column}]"))
+        }
+
+        return lst
+  }
+
+  public List<String> getDataForColumnWithCache(int column){
+        int nrow = getTableMaxRowNum("issueResultWithCache")
+        if(nrow > 20) nrow = 20
+        List<String> lst = new ArrayList<String>()
+        for(int i=1; i<nrow; i++){
+            lst.add(getText("issueResultWithCache[${i}][${column}]"))
+        }
+
+        return lst
+  }
+
+  public String[] getAllText(){
+    return getAllTableCellText("issueResult")
+  }
+
+  public int getTableCellCount(){
+    String sel = getSelector("issueResult")
+    sel = sel + " > tbody > tr > td"
+
+    return getJQuerySelectorCount(sel)
+  }
+
+  public String[] getTableCSS(String name){
+    String[] result = getCSS("issueResult.header[1]", name)
+
+    return result
+  }
+
+  public String[] getIsssueTypes() {
+    return getSelectOptions("issueSearch.issueType")
+  }
+
+  public void selectIssueType(String type) {
+    selectByLabel "issueSearch.issueType", type
+  }
+
+  public void searchIssue(String issue) {
+    keyType "issueSearch.searchBox", issue
+//        type "issueSearch.searchBox", issue
+    click "issueSearch.searchButton"
+    waitForPageToLoad 30000
+  }
+
+  public boolean checkamICacheable(String uid){
+    UiObject obj = getUiElement(uid)
+    boolean cacheable = obj.amICacheable()
+    return cacheable
+  }
+}
