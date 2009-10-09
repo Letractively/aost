e55266fb7f7a815b2307b5a0b17d37b34a555c59
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-10-09 01:23:11 +0000

    allow TelluriumConfig.groovy to be provided from class path

--- trunk/core/src/main/groovy/org/tellurium/config/TelluriumConfigParser.groovy
+++ trunk/core/src/main/groovy/org/tellurium/config/TelluriumConfigParser.groovy
@@ -19,11 +19,28 @@ class TelluriumConfigParser {
        try{
    	    	i18nManager.createDefaultResourceBundle(Locale.getDefault());
             println i18nManager.translate("TelluriumConfigParser.parseConfigFileText" , {fileName})
-            conf = new ConfigSlurper().parse(new File(fileName).toURL())
+            conf = new ConfigSlurper().parse(new File(fileName).toURL())              
        }catch(Exception e){
             conf = null
             println i18nManager.translate("TelluriumConfigParser.cannotOpenConfigFile" , {e.getMessage()})
        }
     }
 
+    public void parse(File file){
+       try{
+          conf = new ConfigSlurper().parse(file.toURL())
+       }catch(Exception e){
+          conf = null
+          println "Cannot open Configuration File"
+       }
+    }
+
+    public void parse(URL url){
+       try{
+          conf = new ConfigSlurper().parse(url)
+       }catch(Exception e){
+          conf = null
+          println "Cannot open Configuration File"
+       }
+    }
 }
