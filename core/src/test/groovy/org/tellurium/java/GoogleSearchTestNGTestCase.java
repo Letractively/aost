79ee95afb4954ca3f1aae2cc5f76f58560447a6f
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-12-08 17:14:56 +0000

    update test cases

---
+++ core/src/test/groovy/org/tellurium/java/GoogleSearchTestNGTestCase.java
@@ -0,0 +1,127 @@
+package org.tellurium.java;
+
+import org.tellurium.test.java.TelluriumTestNGTestCase;
+import org.tellurium.module.GoogleSearchModule;
+import org.tellurium.framework.CachePolicy;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+import static org.testng.Assert.*;
+
+
+/**
+ *
+ * @author: Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: Dec 8, 2009
+ *
+ */
+public class GoogleSearchTestNGTestCase   extends TelluriumTestNGTestCase {
+    private static GoogleSearchModule gsm;
+    private static String te_ns = "http://tellurium.org/ns";
+
+    @BeforeClass
+    public static void initUi() {
+        gsm = new GoogleSearchModule();
+        gsm.defineUi();
+        connectSeleniumServer();
+        useJQuerySelector(true);
+        useTelluriumApi(true);
+        useTrace(true);
+//        useCache(true);
+    }
+
+    @BeforeMethod
+    public void connectToGoogle() {
+
+        connectUrl("http://www.google.com");
+    }
+
+    @Test
+    public void testGoogleSearch() {
+        gsm.doGoogleSearch("tellurium . ( Groovy ) Test");
+    }
+
+    @Test
+    public void testGoogleSearchFeelingLucky() {
+        gsm.doImFeelingLucky("tellurium automated Testing");
+    }
+
+    @Test
+    public void testLogo(){
+        String alt = gsm.getLogoAlt();
+        assertNotNull(alt);
+        assertEquals("Google", alt);
+    }
+
+    @Test
+    public void testIsDisabled(){
+        useJQuerySelector(true);
+        boolean result = gsm.isInputDisabled();
+        assertFalse(result);
+        useJQuerySelector(false);
+        result = gsm.isInputDisabled();
+        assertFalse(result);
+    }
+
+    @Test
+    public void testUseSelectorCache(){
+        useCache(true);
+        boolean result = gsm.getSelectorCacheState();
+        assertTrue(result);
+
+        useCache(false);
+        result = gsm.getSelectorCacheState();
+        assertFalse(result);
+    }
+
+    @Test
+    public void testTypeRepeated(){
+        gsm.doTypeRepeated("tellurium jQuery");
+    }
+
+    @Test
+    public void testRegisterNamespace(){
+        registerNamespace("te", te_ns);
+        String ns = getNamespace("te");
+        assertNotNull(ns);
+        assertEquals(te_ns, ns);
+        ns = getNamespace("x");
+        assertNotNull(ns);
+        assertEquals("http://www.w3.org/1999/xhtml", ns);
+        ns = getNamespace("mathml");
+        assertNotNull(ns);
+        assertEquals("http://www.w3.org/1998/Math/MathML", ns);
+    }
+
+    @Test
+    public void testCachePolicy(){
+        useJQuerySelector(true);
+        useCache(true);
+        String policy = getCurrentCachePolicy();
+        assertEquals("DiscardOldPolicy", policy);
+//        gsm.useDiscardLeastUsedCachePolicy();
+        useCachePolicy(CachePolicy.DISCARD_LEAST_USED);
+        policy = getCurrentCachePolicy();
+        assertEquals("DiscardLeastUsedPolicy", policy);
+//        gsm.useDiscardInvalidCachePolicy();
+        useCachePolicy(CachePolicy.DISCARD_INVALID);
+        policy = getCurrentCachePolicy();
+        assertEquals("DiscardInvalidPolicy", policy);
+//        gsm.useDiscardNewCachePolicy();
+        useCachePolicy(CachePolicy.DISCARD_NEW);
+        policy = getCurrentCachePolicy();
+        assertEquals("DiscardNewPolicy", policy);
+//        gsm.useDiscardOldCachePolicy();
+        useCachePolicy(CachePolicy.DISCARD_OLD);
+        policy = getCurrentCachePolicy();
+        assertEquals("DiscardOldPolicy", policy);
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        showTrace();
+    }
+
+}
