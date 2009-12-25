8a3bd396b6ae5901acd6c39477bf3bdf8a456381
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2009-12-25 03:22:32 +0000

    fix ui module in Jetty Logon example, seems trump did not set the direct attribute correctly

--- core/src/test/resources/jetty/test/js/selenium-mock.js
+++ core/src/test/resources/jetty/test/js/selenium-mock.js
@@ -2,4 +2,21 @@ function Selenium(){
 
 };
 
+function SeleniumError(message) {
+    var error = new Error(message);
+    if (typeof(arguments.caller) != 'undefined') { // IE, not ECMA
+        var result = '';
+        for (var a = arguments.caller; a != null; a = a.caller) {
+            result += '> ' + a.callee.toString() + '\n';
+            if (a.caller == a) {
+                result += '*';
+                break;
+            }
+        }
+        error.stack = result;
+    }
+    error.isSeleniumError = true;
+    return error;
+}
+
 var selenium = new Selenium();
