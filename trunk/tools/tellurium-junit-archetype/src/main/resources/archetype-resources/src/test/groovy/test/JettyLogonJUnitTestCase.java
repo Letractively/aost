76942a95524af3b2699e05246f894ebe50145527
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-01-04 15:50:58 +0000

    add Jetty Logon tests to JUnit Maven archetype

---
+++ trunk/tools/tellurium-junit-archetype/src/main/resources/archetype-resources/src/test/groovy/test/JettyLogonJUnitTestCase.java
@@ -0,0 +1,76 @@
+package test;
+
+import org.telluriumsource.test.java.TelluriumJUnitTestCase;
+import org.telluriumsource.test.mock.MockHttpServer;
+import module.JettyLogonModule;
+import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.AfterClass;
+
+/**
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ *         Date: Jan 3, 2010
+ *
+ */
+public class JettyLogonJUnitTestCase  extends TelluriumJUnitTestCase {
+    private static module.JettyLogonModule jlm;
+    private static MockHttpServer server;
+
+    @BeforeClass
+    public static void initUi() {
+        server = new MockHttpServer(8080);
+        server.registerHtmlBody("/logon.html", JettyLogonModule.HTML_BODY);
+        server.start();
+
+        jlm = new  JettyLogonModule();
+        jlm.defineUi();
+        connectSeleniumServer();
+        useCssSelector(true);
+        useTelluriumApi(true);
+        useTrace(true);
+        useCache(true);
+    }
+
+    @Before
+    public void connectToLocal() {
+        connectUrl("http://localhost:8080/logon.html");
+    }
+
+    @Test
+    public void testJsonfyUiModule(){
+        String json = jlm.jsonify("Form");
+        System.out.println(json);
+    }
+
+    @Test
+    public void testDiagnose(){
+        jlm.diagnose("Form.Username.Input");
+        jlm.diagnose("ProblematicForm.Username.Input");
+    }
+
+    @Test
+    public void testValidateUiModule(){
+        jlm.validateUiModule("Form");
+        jlm.validateUiModule("ProblematicForm");
+    }
+
+    @Test
+    public void testLogon() {
+        jlm.logon("test", "test");
+    }
+
+    @Test
+    public void testLogonWithClosestMatch() {
+        useClosestMatch(true);
+        jlm.plogon("test", "test");
+        useClosestMatch(false);
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        server.stop();
+        showTrace();
+    }
+}
