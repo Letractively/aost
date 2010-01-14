e64e6002c025a8462e16b3d6570ac92f1b16653d
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-01-14 18:32:09 +0000

    add Mock test case

--- trunk/core/src/test/groovy/org/telluriumsource/java/TelluriumIssueTestCase.java
+++ trunk/core/src/test/groovy/org/telluriumsource/java/TelluriumIssueTestCase.java
@@ -1,22 +1,30 @@
 package org.telluriumsource.java;
 
 import org.telluriumsource.test.java.TelluriumJUnitTestCase;
+import org.telluriumsource.test.java.TelluriumMockJUnitTestCase;
 import org.telluriumsource.module.TelluriumIssueModule;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Before;
 
 /**
  * @author Jian Fang (John.Jian.Fang@gmail.com)
  *
  *         Date: Jan 6, 2010
  */
-public class TelluriumIssueTestCase extends TelluriumJUnitTestCase {
+public class TelluriumIssueTestCase extends TelluriumMockJUnitTestCase {
     private static TelluriumIssueModule tim;
 
     @BeforeClass
     public static void setup(){
         tim = new TelluriumIssueModule();
         tim.defineUi();
+        registerTest("TelluriumIssue");
+    }
+
+    @Before
+    public void connect(){
+        connect("TelluriumIssue");
     }
 
     @Test
