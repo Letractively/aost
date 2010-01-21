aa009cb7a30ba42cffed2219967cc39daa14f561
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-01-21 06:03:24 +0000

    Add grapeConfig.xml and update rundsl.groovy

--- trunk/core/rundsl.groovy
+++ trunk/core/rundsl.groovy
@@ -1,13 +1,13 @@
+import org.telluriumsource.dsl.DslScriptExecutor
 
-@Grab(group='org.codehaus.groovy', module='groovy-all', version='1.7.0')
-@Grab(group='org.seleniumhq.selenium.server', module='selenium-server', version='1.0.1-te2')
-@Grab(group='org.seleniumhq.selenium.client-drivers', module='selenium-java-client-driver', version='1.0.1')
-@Grab(group='junit', module='junit', version='4.7')
-@Grab(group='caja', module='json_simple', version='r1')
-@Grab(group='org.apache.poi', module='poi', version='3.0.1-FINAL')
-@Grab(group='org.stringtree', module='stringtree-json', version='2.0.10')
-@Grab(group='org.telluriumsource', module='tellurium-core', version='0.7.0-SNAPSHOT')
-
+@Grapes([@Grab(group='org.codehaus.groovy', module='groovy-all', version='1.7.0'),
+@Grab(group='org.seleniumhq.selenium.server', module='selenium-server', version='1.0.1-te2'),
+@Grab(group='org.seleniumhq.selenium.client-drivers', module='selenium-java-client-driver', version='1.0.1'),
+@Grab(group='junit', module='junit', version='4.7'),
+@Grab(group='caja', module='json_simple', version='r1'),
+@Grab(group='org.apache.poi', module='poi', version='3.0.1-FINAL'),
+@Grab(group='org.stringtree', module='stringtree-json', version='2.0.10'),
+@Grab(group='org.telluriumsource', module='tellurium-core', version='0.7.0-SNAPSHOT')])
 def runDsl(String[] args) {
   def cli = new CliBuilder(usage: 'rundsl.groovy -[hf] [scriptname]')
   cli.with {
@@ -26,8 +26,8 @@ def runDsl(String[] args) {
     def extraArguments = options.arguments()
     if (extraArguments) {
       extraArguments.each {String name ->
-        def input = [name]
-        org.telluriumsource.dsl.DslScriptExecutor.main(input)
+        def input = [name].toArray(new String[0])
+        DslScriptExecutor.main(input)
       }
     }
   }
