85a5b183e6ac7fe2ec80e75d44449fdae8e67c03
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-01-22 16:33:00 +0000

    add example for general table

---
+++ core/src/test/groovy/org/telluriumsource/ft/GeneralTableJUnitTestCase.java
@@ -0,0 +1,59 @@
+package org.telluriumsource.ft;
+
+import org.telluriumsource.test.java.TelluriumMockJUnitTestCase;
+import org.telluriumsource.module.GeneralTableModule;
+import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.AfterClass;
+
+/**
+ * Test StandardTable with more general layout
+ * 
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ *         Date: Jan 22, 2010
+ */
+public class GeneralTableJUnitTestCase extends TelluriumMockJUnitTestCase {
+    private static GeneralTableModule gtm;
+
+    @BeforeClass
+    public static void initUi() {
+        registerHtmlBody("GeneralTable");
+
+        gtm = new GeneralTableModule();
+        gtm.defineUi();
+        useCssSelector(true);
+//        useTelluriumApi(true);
+        useTrace(true);
+
+    }
+
+    @Before
+    public void connectToLocal() {
+        connect("GeneralTable");
+    }
+
+    @Test
+    public void testValidateUiModule(){
+        gtm.validate("GT");
+    }
+
+    @Test
+    public void testWork(){
+        useCache(false);
+        System.out.println(gtm.getText("GT[1][1]"));
+        gtm.work("Tellurium jQuery");
+    }
+
+    @Test
+    public void testWorkWithCache(){
+        useCache(true);
+        gtm.work("Tellurium jQuery");
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        showTrace();
+    }
+}
