399955df417e1ae7c397e975fcc8960dc46f7e7b
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-02-03 07:26:48 +0000

    add ui module

--- core/src/test/groovy/org/telluriumsource/module/JListModule.groovy
+++ core/src/test/groovy/org/telluriumsource/module/JListModule.groovy
@@ -36,11 +36,23 @@ class JListModule extends DslContext {
     }
 
     ui.Form(uid: "SailingForm", clocator: [name: "selectedSailingsForm"] ){
-      List(uid: "Section", clocator: [tag: "div", class: "segment clearfix"]){
-        List(uid: "Option", clocator: [tag: "div", class: "option"]){
-
+      Repeat(uid: "Section", clocator: [tag: "div", class: "segment clearfix"]){
+        Repeat(uid: "Option", clocator: [tag: "div", class: "option"]){
+          List(uid: "Fares", clocator: [tag: "ul", class: "fares"], separator: "li"){
+            Container(uid: "all"){
+                RadioButton(uid: "radio", clocator: [:], respond: ["click"])
+                TextBox(uid: "label", clocator: [tag: "label"])
+            }
+          }
+          Container(uid: "Details", clocator: [tag: "div", class: "details"]){
+            Container(uid: "Photo", clocator: [tag: "div", class: "photo"]){
+              Image(uid: "Image", clocator: [:])
+            }
+            List(uid: "ShipInfo", clocator: [tag: "dl"]){
+              
+            }
+          }
         }
-
       }
     }
   }
