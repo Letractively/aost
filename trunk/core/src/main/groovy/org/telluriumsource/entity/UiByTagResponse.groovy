7da7572026cadc3b5619cfb6aa727568d11738b1
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-02-07 07:53:21 +0000

    add getUiByTag interface

---
+++ trunk/core/src/main/groovy/org/telluriumsource/entity/UiByTagResponse.groovy
@@ -0,0 +1,18 @@
+package org.telluriumsource.entity
+
+/**
+ * 
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: Feb 7, 2010
+ * 
+ */
+class UiByTagResponse {
+
+  String tag;
+
+  int index;
+
+  String uid;
+
+}
