9b557366c1c4aa9ac78ef53b71057beb20d0d4c6
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-02-08 06:50:45 +0000

    add container examples

---
+++ reference-projects/tellurium-examples/src/test/groovy/org.telluriumsource.test/ContainerExampleTestCase.java
@@ -0,0 +1,57 @@
+package org.telluriumsource.test;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.telluriumsource.module.ContainerExampleModule;
+import org.telluriumsource.test.java.TelluriumMockJUnitTestCase;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ *         Date: Feb 8, 2010
+ */
+public class ContainerExampleTestCase extends TelluriumMockJUnitTestCase {
+    private static ContainerExampleModule cem;
+
+    @BeforeClass
+    public static void initUi() {
+        registerHtmlBody("ContainerExample");
+
+        cem = new ContainerExampleModule();
+        cem.defineUi();
+        useCssSelector(true);
+        useTelluriumApi(true);
+        useTrace(true);
+        useCache(true);
+        useMacroCmd(true);
+        useEngineLog(true);
+    }
+
+    @Before
+    public void connectToLocal() {
+        connect("ContainerExample");
+    }
+
+    @Test
+    public void testToHtml(){
+        System.out.println(cem.toHTML("mainnav"));
+    }
+
+    @Test
+    public void testFlowWithTelluriumAndCaching() {
+        cem.click("mainnav.events");
+        cem.waitForPageToLoad(10000);
+        assertTrue(cem.isElementPresent("mainnav"));
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        showTrace();
+    }
+
+}
