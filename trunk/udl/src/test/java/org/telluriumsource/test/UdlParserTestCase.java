1ff470961d79771a43679bfd3c23771ec59fbcc8
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-02-18 22:11:20 +0000

    update test case

--- trunk/udl/src/test/java/org/telluriumsource/test/UdlParserTestCase.java
+++ trunk/udl/src/test/java/org/telluriumsource/test/UdlParserTestCase.java
@@ -1,14 +1,12 @@
 package org.telluriumsource.test;
+
 import org.junit.Test;
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CharStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.TokenStream;
-import org.telluriumsource.udl.MetaData;
-import org.telluriumsource.udl.TableBodyMetaData;
-import org.telluriumsource.udl.UdlLexer;
-import org.telluriumsource.udl.UdlParser;
+import org.telluriumsource.udl.*;
 import org.telluriumsource.udl.code.IndexType;
 
 import static org.junit.Assert.*;
@@ -21,6 +19,42 @@ import static org.junit.Assert.*;
 public class UdlParserTestCase {
 
     @Test
+    public void testBaseUid(){
+ 		CharStream stream =
+			new ANTLRStringStream("Tellurium");
+		UdlLexer lexer = new UdlLexer(stream);
+		TokenStream tokenStream = new CommonTokenStream(lexer);
+		UdlParser parser = new UdlParser(tokenStream);
+        try{
+		    MetaData data = parser.uid();
+            assertNotNull(data);
+            assertEquals("Tellurium", data.getId());
+        }catch(RecognitionException e){
+            fail(e.getMessage());
+        }        
+    }
+
+    @Test
+    public void testListUid(){
+  		CharStream stream =
+			new ANTLRStringStream("{ odd } as T");
+		UdlLexer lexer = new UdlLexer(stream);
+		TokenStream tokenStream = new CommonTokenStream(lexer);
+		UdlParser parser = new UdlParser(tokenStream);
+        try{
+		    MetaData data = parser.uid();
+            assertNotNull(data);
+            assertEquals("T", data.getId());
+            assertTrue(data instanceof ListMetaData);
+            ListMetaData lm = (ListMetaData)data;
+            assertEquals("odd", lm.getIndex().getValue());
+            assertEquals(IndexType.VAL, lm.getIndex().getType());
+        }catch(RecognitionException e){
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
     public void testTableHeaderUid(){
  		CharStream stream =
 			new ANTLRStringStream("{header: 3} as A");
@@ -29,10 +63,35 @@ public class UdlParserTestCase {
 		UdlParser parser = new UdlParser(tokenStream);
         try{
 		    MetaData data = parser.uid();
+            assertNotNull(data);
+            assertTrue(data instanceof TableHeaderMetaData);
+            TableHeaderMetaData th = (TableHeaderMetaData)data;
+            assertEquals("A", th.getId());
+            assertEquals("3", th.getIndex().getValue());
+            assertEquals(IndexType.VAL, th.getIndex().getType());
         }catch(RecognitionException e){
             fail(e.getMessage());
         }
-		System.out.println("ok");
+    }
+
+    @Test
+    public void testTableFooterUid(){
+ 		CharStream stream =
+			new ANTLRStringStream("{footer: all} as B");
+		UdlLexer lexer = new UdlLexer(stream);
+		TokenStream tokenStream = new CommonTokenStream(lexer);
+		UdlParser parser = new UdlParser(tokenStream);
+        try{
+		    MetaData data = parser.uid();
+            assertNotNull(data);
+            assertTrue(data instanceof TableHeaderMetaData);
+            TableHeaderMetaData th = (TableHeaderMetaData)data;
+            assertEquals("B", th.getId());
+            assertEquals("all", th.getIndex().getValue());
+            assertEquals(IndexType.VAL, th.getIndex().getType());
+        }catch(RecognitionException e){
+            fail(e.getMessage());
+        }
     }
 
     @Test
@@ -47,13 +106,13 @@ public class UdlParserTestCase {
             assertNotNull(data);
             assertEquals("Search", data.getId());
             assertTrue(data instanceof TableBodyMetaData);
-            TableBodyMetaData tbmd = (TableBodyMetaData)data;
-            assertEquals("1", tbmd.getTbody().getValue());
-            assertEquals(IndexType.VAL, tbmd.getTbody().getType());
-            assertEquals("2", tbmd.getRow().getValue());
-            assertEquals(IndexType.VAL, tbmd.getRow().getType());
-            assertEquals("3", tbmd.getColumn().getValue());
-            assertEquals(IndexType.VAL, tbmd.getColumn().getType());
+            TableBodyMetaData tb = (TableBodyMetaData)data;
+            assertEquals("1", tb.getTbody().getValue());
+            assertEquals(IndexType.VAL, tb.getTbody().getType());
+            assertEquals("2", tb.getRow().getValue());
+            assertEquals(IndexType.VAL, tb.getRow().getType());
+            assertEquals("3", tb.getColumn().getValue());
+            assertEquals(IndexType.VAL, tb.getColumn().getType());
         }catch(RecognitionException e){
             fail(e.getMessage());
         }
