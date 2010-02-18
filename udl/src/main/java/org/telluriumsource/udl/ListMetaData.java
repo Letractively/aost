302acbf785e7d337d4879cc4c210662850d2790e
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-02-18 20:15:07 +0000

    add meta data

---
+++ udl/src/main/java/org/telluriumsource/udl/ListMetaData.java
@@ -0,0 +1,19 @@
+package org.telluriumsource.udl;
+
+/**
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ *         Date: Feb 18, 2010
+ */
+public class ListMetaData extends MetaData {
+
+    protected Index index;
+
+    public Index getIndex() {
+        return index;
+    }
+
+    public void setIndex(Index index) {
+        this.index = index;
+    }
+}
---
+++ udl/src/main/java/org/telluriumsource/udl/TableBodyMetaData.java
@@ -0,0 +1,38 @@
+package org.telluriumsource.udl;
+
+/**
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ *         Date: Feb 18, 2010
+ */
+public class TableBodyMetaData extends MetaData {
+    protected Index tbody;
+
+    protected Index row;
+
+    protected Index column;
+
+    public Index getTbody() {
+        return tbody;
+    }
+
+    public void setTbody(Index tbody) {
+        this.tbody = tbody;
+    }
+
+    public Index getRow() {
+        return row;
+    }
+
+    public void setRow(Index row) {
+        this.row = row;
+    }
+
+    public Index getColumn() {
+        return column;
+    }
+
+    public void setColumn(Index column) {
+        this.column = column;
+    }
+}
