046bfd77e3128cf61f94c735bba75ed8d25a16bd
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-02-19 06:29:10 +0000

    update two dimension object test case

--- core/src/test/groovy/org/telluriumsource/module/TwoDimModule.groovy
+++ core/src/test/groovy/org/telluriumsource/module/TwoDimModule.groovy
@@ -11,10 +11,12 @@ import org.telluriumsource.dsl.DslContext
  */
 class TwoDimModule  extends DslContext {
   public void defineUi(){
-    ui.StandardTable(uid: "Table", clocator: [id: "table"], ht: "div", bt: "div", frt: "div", fct: "div", brt: "div", bct: "div"){
-      TextBox(uid: "footer: all", clocator: [tag: "div"])
-      TextBox(uid: "row: *, column: 1", clocator: [tag: "div"])
-      Image(uid: "row: *, column: 2", clocator: [:])
+    ui.StandardTable(uid: "Table", clocator: [id: "table"], ht: "div", bt: "div", brt: "div", bct: "div"){
+      TextBox(uid: "tbody: 1, row: *, column: 1", clocator: [tag: "div"])
+      Image(uid: "tbody: 1, row: *, column: 2", clocator: [:])
+      TextBox(uid: "tbody: 2, row: *, column: 1", clocator: [tag: "div"])
+      TextBox(uid: "tbody: 2, row: *, column: 2", clocator: [tag: "div"])
+      TextBox(uid: "tbody: 2, row: *, column: 3", clocator: [tag: "div"])
     }
   }
 }
