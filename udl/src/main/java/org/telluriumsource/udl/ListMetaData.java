39d399bcceefd1b7f0e415e5bb2393e3b11796a3
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-02-26 04:17:09 +0000

    add toJSON() to MetaData classes

--- udl/src/main/java/org/telluriumsource/udl/ListMetaData.java
+++ udl/src/main/java/org/telluriumsource/udl/ListMetaData.java
@@ -1,11 +1,23 @@
 package org.telluriumsource.udl;
 
+import org.json.simple.JSONObject;
+
 /**
  * @author Jian Fang (John.Jian.Fang@gmail.com)
  *
  *         Date: Feb 18, 2010
  */
 public class ListMetaData extends MetaData {
+    public static final String INDEX = "index";
+    protected Index index;
+
+    public Index getIndex() {
+        return index;
+    }
+
+    public void setIndex(Index index) {
+        this.index = index;
+    }
 
     public ListMetaData(String id, Index index) {
         super(id);
@@ -17,13 +29,10 @@ public class ListMetaData extends MetaData {
         this.index = new Index(index);
     }
 
-    protected Index index;
+    public JSONObject toJSON() {
+        JSONObject jso = super.toJSON();
+        jso.put(INDEX, this.index.toJSON());
 
-    public Index getIndex() {
-        return index;
-    }
-
-    public void setIndex(Index index) {
-        this.index = index;
+        return jso;
     }
 }
