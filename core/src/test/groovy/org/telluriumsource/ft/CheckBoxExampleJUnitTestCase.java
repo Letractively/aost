58bc0a5a07a718f29b41a21246368086a076e2a5
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-03-01 14:45:30 +0000

    add test cases for CheckBox

--- core/src/test/groovy/org/telluriumsource/ft/CheckBoxExampleJUnitTestCase.java
+++ core/src/test/groovy/org/telluriumsource/ft/CheckBoxExampleJUnitTestCase.java
@@ -1,11 +1,14 @@
 package org.telluriumsource.ft;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.telluriumsource.module.CheckBoxExample;
 import org.telluriumsource.test.java.TelluriumJUnitTestCase;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Jian Fang (John.Jian.Fang@gmail.com)
@@ -20,13 +23,25 @@ public class CheckBoxExampleJUnitTestCase extends TelluriumJUnitTestCase
     public static void initUi() {
         cbx = new CheckBoxExample();
         cbx.defineUi();
+        connectSeleniumServer();
+    }
+
+    @Before
+    public void connectToSamples(){
+        connectUrl("http://www.asp101.com/samples/checkbox.asp");
     }
 
     @Test
     public void testCheckOneBox() {
-        connectSeleniumServer();
-        connectUrl("http://www.asp101.com/samples/checkbox.asp");
         cbx.checkOneBox("root.input0");
         assertEquals("on", cbx.checkBoxValues("root.input0"));
+    }
+
+    @Test
+    public void testIsChecked(){
+        cbx.check("root.input0");
+        assertTrue(cbx.isChecked("root.input0"));
+        cbx.uncheck("root.input0");
+        assertFalse(cbx.isChecked("root.input0"));
     }
 }
