a9861521a136cea5f1d86fdd60c32327ac64209f
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-03-03 21:47:07 +0000

    add Tellurium test cases to Finance manager

---
+++ src/test/groovy/org/telluriumsource/test/AccountTestCase.java
@@ -0,0 +1,53 @@
+package org.telluriumsource.test;
+
+import org.junit.*;
+import org.telluriumsource.module.AccountModule;
+import org.telluriumsource.test.java.TelluriumJUnitTestCase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ *         Date: Mar 3, 2010
+ */
+public class AccountTestCase extends TelluriumJUnitTestCase {
+    private static AccountModule acm;
+    @BeforeClass
+    public static void initUi() {
+        acm = new AccountModule();
+        acm.defineUi();
+        connectSeleniumServer();
+        useCssSelector(true);
+        useTelluriumEngine(true);
+        useTrace(true);
+    }
+
+    @Before
+    public void connectToFinanceManager() {
+        connectUrl("http://localhost:8080/account");
+    }
+
+    @Test
+    public void testCreateAccount(){
+        String name = getName();
+        String balance = "$10,000";
+        String equity = "50%";
+        String renewalDate = "03/20/12";
+        acm.clear();
+        acm.createAccount(name, balance, equity, renewalDate);
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        showTrace();
+    }
+
+    protected String getName(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyMMddHHmmss");
+        String date = dateFormat.format(new Date());
+
+        return "tellurium" + date;
+    }
+}
