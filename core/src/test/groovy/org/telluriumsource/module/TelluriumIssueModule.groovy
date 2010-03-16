e96c5e2cc17d50d6426507c979e4751a1230bd3c
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-03-16 04:29:22 +0000

    update Table ui module

--- core/src/test/groovy/org/telluriumsource/module/TelluriumIssueModule.groovy
+++ core/src/test/groovy/org/telluriumsource/module/TelluriumIssueModule.groovy
@@ -24,45 +24,45 @@ public class TelluriumIssueModule extends DslContext {
     }
 
     ui.Table(uid: "issueResult", clocator: [id: "resultstable", class: "results"], group: "true") {
-      TextBox(uid: "header: 1", clocator: [:])
-      UrlLink(uid: "header: 2", clocator: [text: "*ID"])
-      UrlLink(uid: "header: 3", clocator: [text: "*Type"])
-      UrlLink(uid: "header: 4", clocator: [text: "*Status"])
-      UrlLink(uid: "header: 5", clocator: [text: "*Priority"])
-      UrlLink(uid: "header: 6", clocator: [text: "*Milestone"])
-      UrlLink(uid: "header: 7", clocator: [text: "*Owner"])
-      UrlLink(uid: "header: 9", clocator: [text: "*Summary + Labels"])
-      UrlLink(uid: "header: 10", clocator: [text: "*..."])
+      TextBox(uid: "{header: 1} as h1", clocator: [:])
+      UrlLink(uid: "{header: 2} as h2", clocator: [text: "*ID"])
+      UrlLink(uid: "{header: 3} as h3", clocator: [text: "*Type"])
+      UrlLink(uid: "{header: 4} as h4", clocator: [text: "*Status"])
+      UrlLink(uid: "{header: 5} as h5", clocator: [text: "*Priority"])
+      UrlLink(uid: "{header: 6} as h6", clocator: [text: "*Milestone"])
+      UrlLink(uid: "{header: 7} as h7", clocator: [text: "*Owner"])
+      UrlLink(uid: "{header: 9} as h9", clocator: [text: "*Summary + Labels"])
+      UrlLink(uid: "{header: 10} as h10", clocator: [text: "*..."])
 
       //define table elements
       //for the border column
-      TextBox(uid: "row: *, column: 1", clocator: [:])
-      TextBox(uid: "row: *, column: 8", clocator: [:])
-      TextBox(uid: "row: *, column: 10", clocator: [:])
+      TextBox(uid: "{row: all, column: 1} as c1", clocator: [:])
+      TextBox(uid: "{row: all, column: 8} as c8", clocator: [:])
+      TextBox(uid: "{row: all, column: 10} as c10", clocator: [:])
       //For the rest, just UrlLink
-      UrlLink(uid: "all", clocator: [:])
+      UrlLink(uid: "{row: all, column: all} as rest", clocator: [:])
     }
 
     ui.Table(uid: "issueResultWithCache", cacheable: "true", noCacheForChildren: "false", clocator: [id: "resultstable", class: "results"], group: "true") {
       //define table header
       //for the border column
-      TextBox(uid: "header: 1", clocator: [:])
-      UrlLink(uid: "header: 2", clocator: [text: "*ID"])
-      UrlLink(uid: "header: 3", clocator: [text: "*Type"])
-      UrlLink(uid: "header: 4", clocator: [text: "*Status"])
-      UrlLink(uid: "header: 5", clocator: [text: "*Priority"])
-      UrlLink(uid: "header: 6", clocator: [text: "*Milestone"])
-      UrlLink(uid: "header: 7", clocator: [text: "*Owner"])
-      UrlLink(uid: "header: 9", clocator: [text: "*Summary + Labels"])
-      UrlLink(uid: "header: 10", clocator: [text: "*..."])
+      TextBox(uid: "{header: 1} as h1", clocator: [:])
+      UrlLink(uid: "{header: 2} as h2", clocator: [text: "*ID"])
+      UrlLink(uid: "{header: 3} as h3", clocator: [text: "*Type"])
+      UrlLink(uid: "{header: 4} as h4", clocator: [text: "*Status"])
+      UrlLink(uid: "{header: 5} as h5", clocator: [text: "*Priority"])
+      UrlLink(uid: "{header: 6} as h6", clocator: [text: "*Milestone"])
+      UrlLink(uid: "{header: 7} as h7", clocator: [text: "*Owner"])
+      UrlLink(uid: "{header: 9} as h9", clocator: [text: "*Summary + Labels"])
+      UrlLink(uid: "{header: 10} as h10", clocator: [text: "*..."])
 
       //define table elements
       //for the border column
-      TextBox(uid: "row: *, column: 1", clocator: [:])
-      TextBox(uid: "row: *, column: 8", clocator: [:])
-      TextBox(uid: "row: *, column: 10", clocator: [:])
+      TextBox(uid: "{row: all, column: 1} as c1", clocator: [:])
+      TextBox(uid: "{row: all, column: 8} as c8", clocator: [:])
+      TextBox(uid: "{row: all, column: 10} as c10", clocator: [:])
       //For the rest, just UrlLink
-      UrlLink(uid: "all", clocator: [:])
+      UrlLink(uid: "{row: all, column: all} as rest", clocator: [:])
     }
 
   }
