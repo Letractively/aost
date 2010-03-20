d6b53f2d5fb856863dcac4677182dc0295a6be8f
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-03-20 05:45:59 +0000

    add SimpleTestNGTest

--- trunk/core/src/main/groovy/org/telluriumsource/component/connector/SeleniumConnector.groovy
+++ trunk/core/src/main/groovy/org/telluriumsource/component/connector/SeleniumConnector.groovy
@@ -14,29 +14,33 @@ import org.telluriumsource.component.bundle.BundleProcessor
  */
 class SeleniumConnector implements Configurable {
 
-    protected int port = 4444
+  protected int port = 4444
 
-	protected final String HTTPS_BASE_URL = "https://localhost:8443"
+  protected final String HTTPS_BASE_URL = "https://localhost:8443"
 
-    protected final String HTTP_BASE_URL = "http://localhost:8080"
+  protected final String HTTP_BASE_URL = "http://localhost:8080"
 
-	protected CustomSelenium sel
+  protected CustomSelenium sel
 
-    protected CustomSelenium customSelenium
+  protected CustomSelenium customSelenium
 
-    protected CommandProcessor commandProcessor
+  protected CommandProcessor commandProcessor
 
-	protected String baseURL = HTTP_BASE_URL
+  protected String baseURL = HTTP_BASE_URL
 
-    protected String browser = "*chrome"
+  protected String browser = "*chrome"
 
-    protected seleniumServerHost = "localhost"
+  protected seleniumServerHost = "localhost"
 
-    protected String userExtension = null
-  
-    protected def customClass = null
+  protected String userExtension = null
 
-    protected String options = null
+  protected def customClass = null
+
+  protected String options = null
+
+  public CustomSelenium getSelenium() {
+    return sel;
+  }
 
   public void connect(String url) {
     sel.open(baseURL + url);
@@ -55,51 +59,51 @@ class SeleniumConnector implements Configurable {
 
   }
 
-    public void configBrowser(String serverHost, int serverPort, String baseUrl, String browser, String browserOptions){
-        this.seleniumServerHost = serverHost;
-        this.port = serverPort;
+  public void configBrowser(String serverHost, int serverPort, String baseUrl, String browser, String browserOptions) {
+    this.seleniumServerHost = serverHost;
+    this.port = serverPort;
 
-        if(baseUrl != null)
-          this.baseURL = baseUrl;
+    if (baseUrl != null)
+      this.baseURL = baseUrl;
 
-        this.browser = browser;
+    this.browser = browser;
 
-        if(browserOptions != null)
-          this.options = browserOptions;
-    }
+    if (browserOptions != null)
+      this.options = browserOptions;
+  }
 
-    public void connectSeleniumServer() {
+  public void connectSeleniumServer() {
 
-        //The selenium server startup logic is moved to EmbeddedSeleniumServer so that we can
-        //decouple the selenium client and the selenium server.
+    //The selenium server startup logic is moved to EmbeddedSeleniumServer so that we can
+    //decouple the selenium client and the selenium server.
 //		if(runSeleniumServerInternally)
 //			setUpSeleniumServer();
-        
-        //Works for https and http
+
+    //Works for https and http
 //        sel = new DefaultSelenium(seleniumServerHost, port, browser, baseURL);
 //        sel = new CustomSelenium(seleniumServerHost, port, browser, baseURL)
-          // CustomSelenium with the new argument CommandProcess
-          // This is done to make sure that implementing the Selenium Grid does
-        // not break the inheritance model for CustomSelenium.
-        customSelenium = new CustomSelenium (commandProcessor)
-        customSelenium.setUserExt(this.userExtension)
-        customSelenium.customClass = this.customClass
-        if(this.options != null && this.options.trim().length() > 0){
-          customSelenium.startSeleniumSession(seleniumServerHost, port, browser, baseURL, this.options)
-        }else{
-          customSelenium.startSeleniumSession(seleniumServerHost, port, browser, baseURL)
-        }
-        sel = customSelenium.getActiveSeleniumSession()
+    // CustomSelenium with the new argument CommandProcess
+    // This is done to make sure that implementing the Selenium Grid does
+    // not break the inheritance model for CustomSelenium.
+    customSelenium = new CustomSelenium(commandProcessor)
+    customSelenium.setUserExt(this.userExtension)
+    customSelenium.customClass = this.customClass
+    if (this.options != null && this.options.trim().length() > 0) {
+      customSelenium.startSeleniumSession(seleniumServerHost, port, browser, baseURL, this.options)
+    } else {
+      customSelenium.startSeleniumSession(seleniumServerHost, port, browser, baseURL)
+    }
+    sel = customSelenium.getActiveSeleniumSession()
 
-        SeleniumClient sc = new SeleniumClient()
-        sc.client = customSelenium
+    SeleniumClient sc = new SeleniumClient()
+    sc.client = customSelenium
 
-        //MK: add the jquery location strategy
+    //MK: add the jquery location strategy
 
-      //use Get to return the DOM reference.  
-      //need to check if it is an attribute locator in the format of locator@attr
+    //use Get to return the DOM reference.
+    //need to check if it is an attribute locator in the format of locator@attr
 
-      //jQuery CSS selector without any Cache mechanism
+    //jQuery CSS selector without any Cache mechanism
 
 /*      sel.addLocationStrategy("jquery", '''
           return tellurium.locateElementByCSSSelector(locator, inDocument, inWindow);
@@ -110,25 +114,25 @@ class SeleniumConnector implements Configurable {
       sel.addLocationStrategy("uimcal", '''
           return tellurium.locateElementWithCacheAware(locator, inDocument, inWindow);
       ''')*/
-    }
+  }
 
-	public void disconnectSeleniumServer() {
-      if(customSelenium != null){
-        CustomSelenium aseles = customSelenium.getActiveSeleniumSession();
-		if(aseles != null){
-            //flush out remaining commands in the command bundle before disconnection
-            BundleProcessor processor = BundleProcessor.instance
-            processor.flush()
-            //clean up cache before close the connection
-            aseles.cleanCache();
-			customSelenium.closeSeleniumSession();
-        }
+  public void disconnectSeleniumServer() {
+    if (customSelenium != null) {
+      CustomSelenium aseles = customSelenium.getActiveSeleniumSession();
+      if (aseles != null) {
+        //flush out remaining commands in the command bundle before disconnection
+        BundleProcessor processor = BundleProcessor.instance
+        processor.flush()
+        //clean up cache before close the connection
+        aseles.cleanCache();
+        customSelenium.closeSeleniumSession();
       }
-	}
-
-    protected void initSeleniumClient(){
-        SeleniumClient sc = new SeleniumClient()
-        sc.client = customSelenium;
     }
+  }
+
+  protected void initSeleniumClient() {
+    SeleniumClient sc = new SeleniumClient()
+    sc.client = customSelenium;
+  }
 
 }
