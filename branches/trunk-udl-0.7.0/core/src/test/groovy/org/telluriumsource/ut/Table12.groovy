c49a7a9051d51ade59b174c1df03f572d1b58c1b
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-03-29 19:09:58 +0000

    Fix Table unit tests

--- branches/trunk-udl-0.7.0/core/src/test/groovy/org/telluriumsource/ut/Table12.groovy
+++ branches/trunk-udl-0.7.0/core/src/test/groovy/org/telluriumsource/ut/Table12.groovy
@@ -13,16 +13,16 @@ import org.telluriumsource.dsl.DslContext
 public class Table12 extends DslContext{
   public void defineUI(){
     ui.Table(uid: "OrderDetail", clocator: [tag: "table", class: "table"]){
-      TextBox(uid: "{row: 1, column: 1}", clocator: [tag: "td", text: "PO Number", class: "tablecolumnheader"])
-      TextBox(uid: "{row: 2, column: 1}", clocator: [tag: "td", text: "Buyer", class: "tablecolumnheader"])
-      TextBox(uid: "{row: 3, column: 1}", clocator: [tag: "td", text: "Vendor", class: "tablecolumnheader"])
-      TextBox(uid: "{row: 4, column: 1}", clocator: [tag: "td", text: "Department", class: "tablecolumnheader"])
-      TextBox(uid: "{row: 5, column: 1}", clocator: [tag: "td", text: "Order Date", class: "tablecolumnheader"])
-      TextBox(uid: "{row: 1, column: 2}", clocator: [tag: "td", text: "Extended Cost", class: "tablecolumnheader"])
-      TextBox(uid: "{row: 2, column: 2}", clocator: [tag: "td", text: "Extended Retail", class: "tablecolumnheader"])
-      TextBox(uid: "{row: 3, column: 3}", clocator: [tag: "td", text: "Markup %", class: "tablecolumnheader"])
-      TextBox(uid: "{row: 4, column: 4}", clocator: [tag: "td", text: "Days from ", class: "tablecolumnheader"])
-      TextBox(uid: "{row: 5, column: 5}", clocator: [tag: "td", text: "Status", class: "tablecolumnheader"])
+      TextBox(uid: "{row: 1, column: 1}", clocator: [tag: "td", text: "PO Number", class: "tablecolumnheader"], self: true)
+      TextBox(uid: "{row: 2, column: 1}", clocator: [tag: "td", text: "Buyer", class: "tablecolumnheader"], self: true)
+      TextBox(uid: "{row: 3, column: 1}", clocator: [tag: "td", text: "Vendor", class: "tablecolumnheader"], self: true)
+      TextBox(uid: "{row: 4, column: 1}", clocator: [tag: "td", text: "Department", class: "tablecolumnheader"], self: true)
+      TextBox(uid: "{row: 5, column: 1}", clocator: [tag: "td", text: "Order Date", class: "tablecolumnheader"], self: true)
+      TextBox(uid: "{row: 1, column: 2}", clocator: [tag: "td", text: "Extended Cost", class: "tablecolumnheader"], self: true)
+      TextBox(uid: "{row: 2, column: 2}", clocator: [tag: "td", text: "Extended Retail", class: "tablecolumnheader"], self: true)
+      TextBox(uid: "{row: 3, column: 3}", clocator: [tag: "td", text: "Markup %", class: "tablecolumnheader"], self: true)
+      TextBox(uid: "{row: 4, column: 4}", clocator: [tag: "td", text: "Days from ", class: "tablecolumnheader"], self: true)
+      TextBox(uid: "{row: 5, column: 5}", clocator: [tag: "td", text: "Status", class: "tablecolumnheader"], self: true)
     }
 
     ui.Table(uid: "chromeDoctorSearchResultsresultstable", clocator: [id: "resultsTable"], group: "true", namespace: "html") {
