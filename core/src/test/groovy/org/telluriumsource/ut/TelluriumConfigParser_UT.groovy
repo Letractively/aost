f7953a4a38091774f6da6db32c28d0386c714ae0
ajay.ravichandran <ajay.ravichandran@gmail.com>
2010-04-05 03:47:36 +0000

    telluriumConfigurator.parse does not use file as argument any more but instead uses the file name

--- core/src/test/groovy/org/telluriumsource/ut/TelluriumConfigParser_UT.groovy
+++ core/src/test/groovy/org/telluriumsource/ut/TelluriumConfigParser_UT.groovy
@@ -13,7 +13,7 @@ class TelluriumConfigParser_UT extends GroovyTestCase{
 
     public void testParse(){
         TelluriumConfigParser parser = new TelluriumConfigParser()
-        parser.parse(ClassLoader.getSystemResource("config/TelluriumConfigForExcelReader.groovy").getFile())
+        parser.parse("config/TelluriumConfigForExcelReader.groovy")
         def config = parser.getProperty("conf")
         assertNotNull(config)
         assertEquals("4444", config.tellurium.embeddedserver.port)
