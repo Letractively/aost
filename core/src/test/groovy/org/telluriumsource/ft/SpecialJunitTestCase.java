64bbc47d8c04a18b87425978a5b8f67c8fc66848
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-04-06 21:27:28 +0000

    add special test case

---
+++ core/src/test/groovy/org/telluriumsource/ft/SpecialJunitTestCase.java
@@ -0,0 +1,53 @@
+package org.telluriumsource.ft;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.telluriumsource.module.SpecialModule;
+import org.telluriumsource.test.java.TelluriumMockJUnitTestCase;
+
+/**
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ *         Date: Apr 6, 2010
+ */
+public class SpecialJunitTestCase extends TelluriumMockJUnitTestCase {
+    private static SpecialModule sm;
+
+    @BeforeClass
+    public static void initUi() {
+        registerHtml("Special");
+
+        sm = new SpecialModule();
+        sm.defineUi();
+//        useTelluriumEngine(true);
+        useTrace(true);
+        useEngineLog(true);
+    }
+
+    @Before
+    public void connectToLocal() {
+        connect("Special");
+    }
+
+    @Test
+    public void testValidateUiModule(){
+        sm.validate("Form");
+    }
+
+    @Test
+    public void testGetHTMLSource(){
+        sm.getHTMLSource("Form");
+    }
+
+    @Test
+    public void testCheck(){
+        sm.check("US", "123456789");
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        showTrace();
+    }
+}
