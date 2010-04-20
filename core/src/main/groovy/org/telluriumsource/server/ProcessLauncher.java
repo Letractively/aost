458a7206a88b080f252767f5031192a2bd8b6738
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-04-20 06:02:28 +0000

    try to add Xvfb

--- core/src/main/groovy/org/telluriumsource/server/ProcessLauncher.java
+++ core/src/main/groovy/org/telluriumsource/server/ProcessLauncher.java
@@ -6,6 +6,101 @@ import groovy.lang.Closure;
 
     http://grepcode.com/file/repo1.maven.org/maven2/org.codehaus.groovy.maven/gmaven-mojo-support/1.0/org/codehaus/groovy/maven/mojo/support/ProcessLauncher.java
 
+    https://svn.apache.org/repos/asf/geronimo/server/tags/2.1.2/framework/modules/geronimo-commands/src/main/groovy/org/apache/geronimo/commands/ProcessLauncher.groovy
+
+class ProcessLauncher
+{
+    private Logger log = LoggerFactory.getLogger(this.class)
+
+    IO io
+
+    String name
+
+    Closure process
+
+    Closure verifier
+
+    int verifyWaitDelay = 1000
+
+    int timeout = -1
+
+    boolean background = false
+
+    def launch() {
+        assert io
+        assert process
+        assert name
+
+        Throwable error
+
+        def runner = {
+            try {
+                process()
+            }
+            catch (Exception e) {
+                error = e
+            }
+        }
+
+        def t = new Thread(runner, "$name Runner")
+
+        io.out.println("Launching ${name}...")
+        io.flush()
+
+        def watch = new StopWatch()
+        watch.start()
+
+        t.start()
+
+        if (verifier) {
+            def timer = new Timer("$name Timer", true)
+
+            def timedOut = false
+
+            def timeoutTask
+            if (timeout > 0) {
+                timeoutTask = timer.runAfter(timeout * 1000, {
+                    timedOut = true
+                })
+            }
+
+            def started = false
+
+            log.debug("Waiting for ${name}...")
+
+            while (!started) {
+                if (timedOut) {
+                    throw new Exception("Unable to verify if $name was started in the given time ($timeout seconds)")
+                }
+
+                if (error) {
+                    throw new Exception("Failed to start: $name", error)
+                }
+
+                if (verifier()) {
+                    started = true
+                }
+                else {
+                    Thread.sleep(verifyWaitDelay)
+                }
+            }
+
+            timeoutTask?.cancel()
+        }
+
+        io.out.println("$name started in $watch")
+        io.flush()
+
+        if (!background) {
+            log.debug("Waiting for $name to shutdown...")
+
+            t.join()
+
+            log.debug("$name has shutdown")
+        }
+    }
+}
+
  */
 public class ProcessLauncher implements groovy.lang.GroovyObject {
     private java.lang.String name = null;
