829866a4ad004a0eb4f15e48eb2613def0a5e76a
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-04-20 05:43:46 +0000

    try to add Xvfb

---
+++ trunk/core/src/main/groovy/org/telluriumsource/server/ProcessLauncher.java
@@ -0,0 +1,85 @@
+package org.telluriumsource.server;
+
+import groovy.lang.Closure;
+
+/*
+
+    http://grepcode.com/file/repo1.maven.org/maven2/org.codehaus.groovy.maven/gmaven-mojo-support/1.0/org/codehaus/groovy/maven/mojo/support/ProcessLauncher.java
+
+ */
+public class ProcessLauncher extends java.lang.Object implements groovy.lang.GroovyObject {
+    private java.lang.String name = null;
+    public java.lang.String getName() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setName(java.lang.String value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    private Closure process = null;
+    public Closure getProcess() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setProcess(Closure value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    private Closure verifier = null;
+    public Closure getVerifier() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setVerifier(Closure value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    private int verifyWaitDelay = 0;
+    public int getVerifyWaitDelay() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setVerifyWaitDelay(int value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    private int timeout = 0;
+    public int getTimeout() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setTimeout(int value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    private boolean background = false;
+    public boolean getBackground() {
+        throw new InternalError("Stubbed method");
+    }
+    public boolean isBackground() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setBackground(boolean value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.Object launch() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public groovy.lang.MetaClass getMetaClass() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void setMetaClass(groovy.lang.MetaClass metaClass) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.Object invokeMethod(java.lang.String name, java.lang.Object args) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.Object getProperty(java.lang.String name) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void setProperty(java.lang.String name, java.lang.Object value) {
+        throw new InternalError("Stubbed method");
+    }
+}
