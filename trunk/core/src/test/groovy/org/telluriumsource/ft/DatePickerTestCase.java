9ffd16675fbf9510dd13bd184583fae240d9bb2a
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-05-28 17:16:06 +0000

    add date picker module

--- trunk/core/src/test/groovy/org/telluriumsource/ft/DatePickerTestCase.java
+++ trunk/core/src/test/groovy/org/telluriumsource/ft/DatePickerTestCase.java
@@ -3,6 +3,7 @@ package org.telluriumsource.ft;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.telluriumsource.module.JQueryDatePicker;
 import org.telluriumsource.test.java.TelluriumJUnitTestCase;
 import org.telluriumsource.test.mock.MockFileServer;
 
@@ -24,7 +25,13 @@ public class DatePickerTestCase extends TelluriumJUnitTestCase{
     public void testShowDatePicker(){
         connectSeleniumServer();
         connectUrl("http://localhost:8088/datepicker/datepicker.html");
-        int delay = 5000;
+        JQueryDatePicker datePicker = new JQueryDatePicker();
+        datePicker.defineUi();
+        datePicker.click("DatePickerInput");
+        datePicker.next();
+        datePicker.selectDay(20);
+        String date = datePicker.getText("DatePickerInput");
+        System.out.println("Selected Date: " + date);
     }
 
     @AfterClass
