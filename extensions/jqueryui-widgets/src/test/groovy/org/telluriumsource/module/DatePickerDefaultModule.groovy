9b698a14a3b8b6181ac4e3bf1c320f81fa99afe2
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-05-28 21:19:00 +0000

    add jqueryui widget

---
+++ extensions/jqueryui-widgets/src/test/groovy/org/telluriumsource/module/DatePickerDefaultModule.groovy
@@ -0,0 +1,45 @@
+package org.telluriumsource.module
+
+import org.telluriumsource.dsl.DslContext
+
+/**
+ * 
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: May 28, 2010
+ * 
+ */
+class DatePickerDefaultModule extends DslContext {
+    public void defineUi() {
+       ui.InputBox(uid: "DatePickerInput", clocator: [id: "datepicker"])
+       ui.JQuery_DatePicker(uid: "Default", clocator: [tag: "body"])
+    }
+
+  public void prevMonth(){
+    onWidget "Default", click, "DatePicker.Header.Prev"
+  }
+
+  public void nextMonth(){
+    onWidget "Default", click, "DatePicker.Header.Next"
+  }
+
+  public void selectDay(int day){
+    onWidget "Default", selectDay, day
+  }
+
+  public void selectFriday(int week){
+    onWidget "Default", selectFriday, week
+  }
+
+  public String getMonthYear(){
+    onWidget "Default", getMonthYear
+  }
+ 
+  public void toggleInput(){
+    click "DatePickerInput"
+  }
+
+  public String getSelectedDate(){
+    getValue "DatePickerInput"
+  }
+}
