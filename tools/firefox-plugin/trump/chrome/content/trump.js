98cece83ae794fe73dd9635d057bc43f22d04604
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-07-07 04:28:56 +0000

    remove unnecessary ui

--- tools/firefox-plugin/trump/chrome/content/trump.js
+++ tools/firefox-plugin/trump/chrome/content/trump.js
@@ -1,12 +1,14 @@
 
 
 var trumpWindow;
+/*
 //opens or update the xpatherBowser window
 function openUIModelWindow(clickedNode){
     //window opens asynchronously... so you cannot call methods immediately
     trumpWindow = window.open("chrome://trump/content/trumpBrowser.xul","uimodel_results","chrome,centerscreen,alwaysRaised=true,resizable");
 
 }
+*/
 
 //opens Trump IDE Window
 function openTrumpIDEWindow(){
