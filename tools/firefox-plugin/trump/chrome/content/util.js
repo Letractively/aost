54da12ee24e6ced58a9a6a294097581bb29e54ac
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-07-09 20:10:45 +0000

    add selenium autocomplete component

--- tools/firefox-plugin/trump/chrome/content/util.js
+++ tools/firefox-plugin/trump/chrome/content/util.js
@@ -68,3 +68,82 @@ function createListCell(value){
 
     return cell;
 }
+
+var XulUtils = {
+    clearChildren: function(e) {
+        var i;
+        for (i = e.childNodes.length - 1; i >= 0; i--) {
+            e.removeChild(e.childNodes[i]);
+        }
+    },
+
+    appendMenuItem: function(e, attributes) {
+        var menuitem = document.createElement("menuitem");
+        for (key in attributes) {
+            if (attributes[key] != null) {
+                menuitem.setAttribute(key, attributes[key]);
+            }
+        }
+        e.appendChild(menuitem);
+    },
+
+    toXPCOMArray: function(data) {
+        var array = Components.classes["@mozilla.org/supports-array;1"].createInstance(Components.interfaces.nsISupportsArray);
+        for (var i = 0; i < data.length; i++) {
+            array.AppendElement(this.toXPCOMString(data[i]));
+        }
+        return array;
+    },
+
+    toXPCOMString: function(data) {
+        var string = Components.classes["@mozilla.org/supports-string;1"].createInstance(Components.interfaces.nsISupportsString);
+        string.data = data;
+        return string;
+    },
+
+    atomService: Components.classes["@mozilla.org/atom-service;1"].
+            getService(Components.interfaces.nsIAtomService)
+};
+
+XulUtils.TreeViewHelper = classCreate();
+objectExtend(XulUtils.TreeViewHelper.prototype, {
+    initialize: function(tree) {
+        tree.view = this;
+        this.tree = tree;
+    },
+
+    scrollToRow: function(index) {
+        this.treebox.ensureRowIsVisible(index);
+    },
+
+    rowUpdated: function(index) {
+        this.treebox.invalidateRow(index);
+    },
+
+    //
+    // nsITreeView interfaces
+    //
+    setTree: function(treebox) {
+        this.log.debug("setTree: treebox=" + treebox);
+        this.treebox = treebox;
+    },
+    isContainer: function(row) {
+        return false;
+    },
+    isSeparator: function(row) {
+        return false;
+    },
+    isSorted: function(row) {
+        return false;
+    },
+    getLevel: function(row) {
+        return 0;
+    },
+    getImageSrc: function(row, col) {
+        return null;
+    },
+    getColumnProperties: function(colid, col, props) {
+    },
+    cycleHeader: function(colID, elt) {
+    }
+});
