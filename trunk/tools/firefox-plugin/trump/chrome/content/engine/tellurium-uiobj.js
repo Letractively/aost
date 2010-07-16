2af486b61ce7241648518d77162d8b6c85a71e52
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-07-16 00:57:36 +0000

    fix url link description

--- trunk/tools/firefox-plugin/trump/chrome/content/engine/tellurium-uiobj.js
+++ trunk/tools/firefox-plugin/trump/chrome/content/engine/tellurium-uiobj.js
@@ -660,8 +660,11 @@ var UiTextBox = UiObject.extend({
 });
 
 var UiUrlLink = UiObject.extend({
-    uiType: 'UrlLink',
-    tag: "a"
+    init: function() {
+        this._super();
+        this.uiType = 'UrlLink';
+        this.tag = "a";
+    }
 });
 
 var UiContainer = UiObject.extend({
