21143dd1b534465850903e2bec26ec2a77310d5b
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-08-06 19:38:38 +0000

    add browserbot to encapsulate browser states

---
+++ tools/firefox-plugin/trump/chrome/content/engine/tellurium-browserbot.js
@@ -0,0 +1,85 @@
+const PageLoadError = {
+    TIMEOUT: "timeout"
+};
+
+var BrowserBot = Class.extend({
+    init: function() {
+        this.currentWindow = null;
+        this.currentWindowName = null;
+        this.newPageLoaded = false;
+        this.pageLoadError = null;
+        this.defaultTimeout = 30000;
+        this.timerId = null;
+    },
+
+    getCurrentWindow: function(){
+        return this.currentWindow;    
+    },
+
+    setCurrentWindow: function(window){
+        this.currentWindow = window;
+    },
+
+    setCurrentWindowToMostRecentWindow: function(){
+        this.currentWindow = this.getMostRecentWindow();
+    },
+    
+    openNewWindow:  function(uid, url) {
+        var win = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                .getService(Components.interfaces.nsIWindowMediator)
+                .getMostRecentWindow("navigator:browser");
+
+        win.open(url);
+    },
+
+    getMostRecentDocument: function() {
+        var win = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                .getService(Components.interfaces.nsIWindowMediator)
+                .getMostRecentWindow("navigator:browser");
+
+        return win.getBrowser().contentDocument;
+    },
+
+    getMostRecentWindow: function() {
+        var win = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                .getService(Components.interfaces.nsIWindowMediator)
+                .getMostRecentWindow("navigator:browser");
+
+        return win.getBrowser().contentWindow;
+    },
+
+    showInBrowser: function(url) {
+        var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+        var window = wm.getMostRecentWindow('navigator:browser');
+        var contentWindow = window.getBrowser().contentWindow;
+        contentWindow.location.href = url;
+    },
+
+    getMainWindow: function() {
+        var mainWindow = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+                .getInterface(Components.interfaces.nsIWebNavigation)
+                .QueryInterface(Components.interfaces.nsIDocShellTreeItem)
+                .rootTreeItem
+                .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+                .getInterface(Components.interfaces.nsIDOMWindow);
+
+        return mainWindow;
+    },
+
+    getAllWindows: function() {
+        var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+        var e = wm.getEnumerator("navigator:browser");
+        var wins = new Array();
+        while (e.hasMoreElements()) {
+            var window = e.getNext();
+            var browsers = window.getBrowser().browsers;
+            for (var i = 0; i < browsers.length; i++) {
+                wins.push(browsers[i].contentWindow);
+            }
+        }
+
+        return wins;
+    }
+});
+
+var browserBot = new BrowserBot();
