3aacf7c471d4b17b0382fe629fe907999fafd769
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-08-18 21:23:41 +0000

    add swingx plugin

--- trunk/tools/tellurium-works/griffon-app/views/telluriumworks/TelluriumworksView.groovy
+++ trunk/tools/tellurium-works/griffon-app/views/telluriumworks/TelluriumworksView.groovy
@@ -1,14 +1,39 @@
 package telluriumworks
 
-application(title: 'telluriumworks',
+import java.awt.Color
+import org.jdesktop.swingx.painter.GlossPainter
+
+gloss = glossPainter(paint: new Color(1f, 1f, 1f, 0.2f),
+    position: GlossPainter.GlossPosition.TOP)
+stripes = pinstripePainter(paint: new Color(1f, 1f, 1f, 0.17f),
+                           spacing: 5f)
+matte = mattePainter(fillPaint: new Color(51, 51, 51))
+compound = compoundPainter(painters: [matte, stripes, gloss])
+
+application(title: 'TelluriumWorks',
   //size: [320,480],
   pack: true,
   //location: [50,50],
   locationByPlatform:true,
-  iconImage: imageIcon('/griffon-icon-48x48.png').image,
-  iconImages: [imageIcon('/griffon-icon-48x48.png').image,
-               imageIcon('/griffon-icon-32x32.png').image,
-               imageIcon('/griffon-icon-16x16.png').image]) {
-    // add content here
-    label('Content Goes Here') // deleteme
+  iconImage: imageIcon('/tellurium.png').image,
+  iconImages: [imageIcon('/tellurium.png').image]) {
+
+    borderLayout()
+    jxheader(constraints: NORTH,
+        title: "Tellurium Works",
+        description: "Tellurium IDE to run Tellurium DSL script",
+        titleForeground: Color.WHITE,
+        descriptionForeground: Color.WHITE,
+        icon: imageIcon("/tellurium.png"),
+        preferredSize: [480,80],
+        backgroundPainter: compound
+    )
+    jxtaskPaneContainer(constraints: CENTER) {
+        jxtaskPane(title: "Task group 1") {
+            jxlabel("Action 1")
+        }
+        jxtaskPane(title: "Task group 2", expanded: false) {
+            label("Action 2")
+        }
+    } 
 }
