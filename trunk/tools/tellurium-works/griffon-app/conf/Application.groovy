6594221b5339bde2d2accaa7456f2ecf47eaf926
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-08-18 21:01:40 +0000

    add telluriumWorks project code base

---
+++ trunk/tools/tellurium-works/griffon-app/conf/Application.groovy
@@ -0,0 +1,19 @@
+application {
+    title = 'Telluriumworks'
+    startupGroups = ['telluriumworks']
+
+    // Should Griffon exit when no Griffon created frames are showing?
+    autoShutdown = true
+
+    // If you want some non-standard application class, apply it here
+    //frameClass = 'javax.swing.JFrame'
+}
+mvcGroups {
+    // MVC Group for "telluriumworks"
+    'telluriumworks' {
+        model = 'telluriumworks.TelluriumworksModel'
+        controller = 'telluriumworks.TelluriumworksController'
+        view = 'telluriumworks.TelluriumworksView'
+    }
+
+}
