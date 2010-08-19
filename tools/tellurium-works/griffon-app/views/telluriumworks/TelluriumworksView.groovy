be0e9c9c31a8c94374da753abc95b884d0a1e266
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-08-19 05:25:56 +0000

    update TeluriumWorks

--- tools/tellurium-works/griffon-app/views/telluriumworks/TelluriumworksView.groovy
+++ tools/tellurium-works/griffon-app/views/telluriumworks/TelluriumworksView.groovy
@@ -9,17 +9,37 @@ stripes = pinstripePainter(paint: new Color(1f, 1f, 1f, 0.17f),
                            spacing: 5f)
 matte = mattePainter(fillPaint: new Color(51, 51, 51))
 compound = compoundPainter(painters: [matte, stripes, gloss])
+actions {
+   action(id: 'openAction',
+      name: 'Open',
+      mnemonic: 'O',
+      accelerator: shortcut('O'),
+      closure: controller.openFile)
+   action(id: 'quitAction',
+      name: 'Quit',
+      mnemonic: 'Q',
+      accelerator: shortcut('Q'),
+      closure: controller.quit)
+}
 
-application(title: 'TelluriumWorks',
-  //size: [320,480],
-  pack: true,
-  //location: [50,50],
-  locationByPlatform:true,
+fileChooserWindow = fileChooser()
+fileViewerWindow = application(title: 'TelluriumWorks',
+  size:[480,320], locationByPlatform:true,
   iconImage: imageIcon('/tellurium.png').image,
   iconImages: [imageIcon('/tellurium.png').image]) {
+   menuBar {
+      menu('File') {
+         menuItem openAction
+         separator()
+         menuItem quitAction
+      }
+   }
 
-    borderLayout()
-    jxheader(constraints: NORTH,
+  borderLayout()
+
+  tabbedPane id: 'tabGroup', constraints: CENTER
+
+/*    jxheader(constraints: NORTH,
         title: "Tellurium Works",
         description: "Tellurium IDE to run Tellurium DSL script",
         titleForeground: Color.WHITE,
@@ -35,5 +55,7 @@ application(title: 'TelluriumWorks',
         jxtaskPane(title: "Task group 2", expanded: false) {
             label("Action 2")
         }
-    } 
+    } */
 }
+
+fileChooserWindow = fileChooser()
