38b0243c7b0d2e7cd76ea87bbeb46a73b173b276
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-08-21 16:37:35 +0000

    update config view

--- tools/tellurium-works/griffon-app/views/telluriumworks/TelluriumworksView.groovy
+++ tools/tellurium-works/griffon-app/views/telluriumworks/TelluriumworksView.groovy
@@ -43,7 +43,7 @@ actions {
 
 fileChooserWindow = fileChooser()
 fileViewerWindow = application(title: 'TelluriumWorks',
-        size: [480, 320], locationByPlatform: true,
+        size: [480, 400], locationByPlatform: true,
         iconImage: imageIcon('/tellurium.png').image,
         iconImages: [imageIcon('/tellurium.png').image]) {
   lookAndFeel('system')
@@ -96,24 +96,24 @@ fileViewerWindow = application(title: 'TelluriumWorks',
     }
 
     panel(title: "Server", id: 'box') {
-        migLayout(layoutConstraints: 'fill')
+      migLayout(layoutConstraints: 'fill')
 
       label('Server')
       buttonGroup(id: 'mode')
       radioButton('local', buttonGroup: mode, selected: true, actionPerformed: {model.mode = 'local'})
       radioButton('remote', buttonGroup: mode, constraints: 'wrap', actionPerformed: {model.mode = 'remote'})
 
-        label('Port:')
-        textField(columns: 20, constraints: 'span 2, wrap',
-            text: bind('serverPort', target: model, mutual: true))
+      label('Port:')
+      textField(columns: 20, constraints: 'span 2, wrap',
+              text: bind('port', target: model, mutual: true))
 
-        label('Profile:')
-        textField(columns: 20, constraints: 'span 2, wrap',
-            text: bind('profile', target: model, mutual: true))
+      label('Profile:')
+      textField(columns: 20, constraints: 'span 2, wrap',
+              text: bind('profile', target: model, mutual: true))
 
-        checkBox(id: "multipleWindow", selected: false, text: "Multiple Windows", constraints: 'span 2,wrap')
+      checkBox(id: "multipleWindow", selected: false, text: "Multiple Windows", constraints: 'span 2,wrap')
 
-        separator(constraints: 'grow, span 3, wrap')
+      separator(constraints: 'grow, span 3, wrap')
 
       button(id: 'runBtn',
               label: "Run",
@@ -129,88 +129,72 @@ fileViewerWindow = application(title: 'TelluriumWorks',
 
     }
 
-    panel(title: "Config", layout: new MigLayout('fill'), border: titledBorder('Tellurium Configuration'), constraints: 'grow') {
-      gridLayout(cols: 1, rows: 7)
+    panel(title: "Config") {
+      migLayout(layoutConstraints: 'fill')
+      label("Browser:")
+      comboBox(id: 'browserType',
+              items: ["*chrome", "*firefox", "*iexplore", "*iehta"],
+              selectedIndex: 0,
+              constraints: "span 2, wrap"
+      )
 
-      hbox() {
-        label(id: 'browserlbl',
-                text: "Browser:",
-                constraints: "shrinkx,height 25px,width 20px"
-        )
-        comboBox(id: 'browserType',
-                items: ["*chrome", "*firefox", "*iexplore", "*iehta"],
-                selectedIndex: 0,
-                constraints: "growx,width 20px"
-        )
-      }
+      label("Server Host:")
 
-      hbox() {
-        hbox() {
-          label(id: 'selServerlbl',
-                  text: "Server Host:",
-                  constraints: "shrinkx,height 25px,width 20px"
-          )
+      textField(id: 'selServerHost',
+              columns: 20,
+              constraints: "span 2, wrap",
+              text: bind('serverHost', target: model, mutual: true)
+      )
 
-          textField(id: 'selServerValue',
-                  constraints: "shrink,height 25px,width 50px"
-          )
+      label("Server Port:")
 
-        }
+      textField(id: 'selServerPort',
+              columns: 20,
+              constraints: "span 2, wrap",
+              text: bind('serverPort', target: model, mutual: true)
+      )
 
-        hbox() {
-          label(id: 'selPortlbl',
-                  text: "Server Port:",
-                  constraints: "shrinkx,height 25px,width 20px"
-          )
+      label("Macro Command:")
 
-          textField(id: 'selPortValue',
-                  constraints: "shrink,height 25px,width 50px"
-          )
-        }
+      textField(id: 'macroCmdValue',
+              columns: 20,
+              constraints: "span 2, wrap",
+              text: bind('macroCmd', target: model, mutual: true)
+      )
 
-      }
+      label("Option:")
 
-      hbox() {
-        label(id: 'maxMacroCmdbl',
-                text: "Macro Command:",
-                constraints: "shrinkx,height 25px,width 20px"
-        )
-
-        textField(id: 'macCmdValue',
-                constraints: "shrink,height 25px,width 50px"
-        )
-      }
-
-      hbox() {
-        label(id: 'optiondbl',
-                text: "option:",
-                constraints: "shrinkx,height 25px,width 20px"
-        )
-
-        textField(id: 'optionValue',
-                constraints: "shrink,height 25px,width 50px"
-        )
-      }
+      textField(id: 'optionValue',
+              columns: 20,
+              constraints: "span 2, wrap",
+              text: bind('option', target: model, mutual: true)
+      )
 
       checkBox(id: "useTrace", selected: false, text: "Trace", constraints: 'wrap')
 
       checkBox(id: "useScreenShot", selected: false, text: "ScreenShot", constraints: 'wrap')
 
+      label("locale:")
+      comboBox(id: 'localeType',
+              items: ["en_US", "fr_FR", "zh_CN"],
+              selectedIndex: 0,
+              constraints: "span 2, wrap"
+      )
       button(id: 'applyBtn',
               label: "Apply",
               actionPerformed: {controller.setTelluriumConfig()},
-              constraints: "width 20px ,shrinkx,wrap"
-      )
+              constraints: "span 2, center")
 
     }
+
   }
 
-   toolBar(constraints: 'left', floatable: false, opaque: false) {
+  toolBar(constraints: 'left', floatable: false, opaque: false) {
     button(goMainPage, constraints: 'left')
     button(goServerPage, constraints: 'left')
     button(goConfigPage, constraints: 'left')
     button(exitAction, constraints: 'left')
-  } 
+  }
 }
 /*  panel(layout: new MigLayout('fill')) {
     panel(layout: new MigLayout('fill'), border: titledBorder('Tellurium Script Runner'), constraints: 'grow 100 1, wrap, height 70%') {
