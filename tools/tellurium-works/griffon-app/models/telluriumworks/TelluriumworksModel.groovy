6a248e24e4572ba62ae9e0d9b47d3ae561a6714a
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-08-21 16:07:31 +0000

    update server view

--- tools/tellurium-works/griffon-app/models/telluriumworks/TelluriumworksModel.groovy
+++ tools/tellurium-works/griffon-app/models/telluriumworks/TelluriumworksModel.groovy
@@ -3,12 +3,13 @@ package telluriumworks
 import groovy.beans.Bindable
 
 class TelluriumworksModel {
-   // @Bindable String propName
-//  @Bindable String fileName = ""
   @Bindable int tabSelected = 1
   @Bindable boolean enabled
   File loadedFile
   @Bindable String fileText
   @Bindable boolean dirty
-  
+  @Bindable String mode
+  @Bindable String serverPort
+  @Bindable String profile
+
 }
