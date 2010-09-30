86c46ee22a633396934be6d789146da37c27f79f
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-09-30 21:14:38 +0000

    add annotations for AST transformation

---
+++ branches/trunk-0.8.0/core/src/main/groovy/org/telluriumsource/annotation/Inject.java
@@ -0,0 +1,23 @@
+package org.telluriumsource.annotation;
+
+import org.codehaus.groovy.transform.GroovyASTTransformationClass;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+
+/**
+ * 
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *
+ * Date: Sep 30, 2010
+ * 
+ */
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.FIELD})
+@GroovyASTTransformationClass("org.telluriumsource.ast.InjectASTTransformation")
+public @interface Inject {
+    String id() default "";
+    boolean lazy() default false;
+}
---
+++ branches/trunk-0.8.0/core/src/main/groovy/org/telluriumsource/ast/SessionASTTransformation.java
@@ -0,0 +1,9 @@
+package org.telluriumsource.ast;
+
+/**
+ * @author Jian Fang (John.Jian.Fang@gmail.com)
+ *         
+ *         Date: Sep 30, 2010
+ */
+public class SessionASTTransformation {
+}
