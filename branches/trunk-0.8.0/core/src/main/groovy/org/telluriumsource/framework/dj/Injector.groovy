3063eb832232cea239fe74d80f552e227d1c4293
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-10-12 05:30:13 +0000

    fix SeleniumConnector

--- branches/trunk-0.8.0/core/src/main/groovy/org/telluriumsource/framework/dj/Injector.groovy
+++ branches/trunk-0.8.0/core/src/main/groovy/org/telluriumsource/framework/dj/Injector.groovy
@@ -9,6 +9,7 @@ import org.telluriumsource.dsl.TelluriumApi
 import org.telluriumsource.framework.SessionManager
 import org.telluriumsource.component.connector.CustomSelenium
 import org.telluriumsource.exception.FrameworkWiringException
+import org.telluriumsource.component.connector.SeleniumConnector
 
 /**
  * 
@@ -83,12 +84,9 @@ class Injector implements SessionAwareBeanFactory{
 
       env.setResourceBundle(i18nBundle);
       addBean(session, "i18nBundle",  IResourceBundle.class, org.telluriumsource.crosscut.i18n.ResourceBundle.class, Scope.Session, true, i18nBundle);
-//    CustomSelenium customSelenium = new CustomSelenium(env.getServerHost(), env.getServerPort(), env.getBrowser(), env.getBaseUrl());
-//    addBean(session, "customSelenium", CustomSelenium.class, CustomSelenium.class, Scope.Session, true, customSelenium);
-//    SeleniumWrapper wrapper = getByClass(session, SeleniumWrapper.class);
+      getByClass(session, SeleniumConnector.class);
       SeleniumWrapper wrapper = getByName(session, "SeleniumWrapper");
       session.wrapper = wrapper;
-//    TelluriumApi api = getByClass(session, TelluriumApi.class);
       TelluriumApi api = getByName(session, "TelluriumApi");
       session.api = api;
     }catch(Exception e){
