7e5ed41181acbc42e2d758b283735afb9ec9acc1
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-10-12 05:07:09 +0000

    MethodPointerExpression still has problem

--- core/src/main/groovy/org/telluriumsource/exception/BeanNotFoundException.java
+++ core/src/main/groovy/org/telluriumsource/exception/BeanNotFoundException.java
@@ -5,17 +5,17 @@ package org.telluriumsource.exception;
  *
  *         Date: Oct 4, 2010
  */
-public class BeanNotFoundException extends TelluriumException{
+public class BeanNotFoundException extends TelluriumException {
 
     protected static final String ERROR_CODE = "TELLURIUM_BEAN_NOT_FOUND_EXCEPTION";
 
-  public BeanNotFoundException(String message) {
-    super(message);
-    this.errorCode = ERROR_CODE;
-  }
+    public BeanNotFoundException(String message) {
+        super(message);
+        this.errorCode = ERROR_CODE;
+    }
 
-  public BeanNotFoundException(String message, Exception e) {
-    super(message, e);
-    this.errorCode = ERROR_CODE;
-  }
+    public BeanNotFoundException(String message, Exception e) {
+        super(message, e);
+        this.errorCode = ERROR_CODE;
+    }
 }
