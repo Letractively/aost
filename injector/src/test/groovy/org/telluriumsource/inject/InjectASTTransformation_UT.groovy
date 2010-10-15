a21d7458e00b912d03b54df68f976ae90458789a
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-10-15 03:14:53 +0000

    fix unit test

--- injector/src/test/groovy/org/telluriumsource/inject/InjectASTTransformation_UT.groovy
+++ injector/src/test/groovy/org/telluriumsource/inject/InjectASTTransformation_UT.groovy
@@ -22,10 +22,20 @@ class InjectASTTransformation_UT extends GroovyShellTestCase {
 
       import org.telluriumsource.annotation.Provider
       import org.telluriumsource.annotation.Inject
-      import org.telluriumsource.inject.Scope
-      import org.telluriumsource.mock.MockInjector
+      import org.telluriumsource.inject.Injector
+      import org.telluriumsource.inject.Bean
 
+      @Inject
+      @Provider
+      class TestInjector extends Injector {
 
+          public String getCurrentSessionId(){
+            return "default"
+          }
+
+      }
+
+      @Provider(name="x")
       public class X {
         private int x = 10;
 
@@ -38,21 +48,32 @@ class InjectASTTransformation_UT extends GroovyShellTestCase {
       public class Y {
         @Inject(name="x", lazy=true)
         private X x
+        private List<Bean> beans;
 
         public int getValue(){
           return x.getValue()
         }
 
       }
-      MockInjector.instance.addBean("x",  X.class, X.class, Scope.Session, true, new X());
-      new Y()
 
+      Y y = new Y()
+      y.beans = TestInjector.instance.getAllBeans()
+
+      y
       """)
 
-      println MockInjector.instance.showAllBeans();
       assertNotNull y
       assertNotNull y.getValue()
       assertEquals 10, y.getValue()
+      List<Bean> beans = y.beans
+      assertNotNull beans
+      assertTrue(beans.size() > 0)
+      println "Bean size: " + beans.size()
+      StringBuffer sb = new StringBuffer(64 * beans.size());
+      sb.append("{");
+      sb.append(beans.join(",\n"));
+      sb.append("}");
+      println sb.toString()
   }
 
 }
