18655a4238e7a285a5627c3a2048b3a869cda64c
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-11-12 04:57:55 +0000

    tag tellurium IDE

---
+++ tags/tellurium-ide-111110/source/defaults/preferences/teide.js
@@ -0,0 +1,4 @@
+pref("extensions.teide.exportdirectory", "c:\\");
+pref("extensions.teide.jslog", "true");
+pref("extensions.teide.logwrap", "true");
+pref("extensions.teide.grouplocating", "true");
