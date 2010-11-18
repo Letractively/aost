1e7a2d84ae619c73f7c9e897af970cc34f8ce04b
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-11-18 20:08:53 +0000

    implement TelluriumFactory as a singleton

--- tools/tellurium-ide/source/chrome/content/engine/tellurium.js
+++ tools/tellurium-ide/source/chrome/content/engine/tellurium.js
@@ -725,4 +725,39 @@ Tellurium.prototype.decorateFunctionWithTimeout = function(f, timeout) {
     };
 };
 
-var tellurium = new Tellurium();
+function TelluriumFactory() {
+  var instance = (function() {
+    var _tellurium = null;
+    var initialized = false;
+
+    function initialize () {
+        _tellurium = new Tellurium();
+        _tellurium.initialize();
+        initialized = true;
+    }
+
+    return {
+      // public interface
+      getInstance: function () {
+          if(!initialized){
+              initialize();
+          }
+
+          return _tellurium;
+      }
+    };
+  })();
+
+  TelluriumFactory = function () {
+    // re-define the function for subsequent calls
+    return instance;
+  };
+
+  return TelluriumFactory(); // call the new function
+}
+
+alert("tellurium before initalization" + tellurium);
+var tellurium = (new TelluriumFactory()).getInstance();
+alert("tellurium after initalization" + tellurium);
+
+//var tellurium = new Tellurium();
