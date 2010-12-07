a96fb8d06f5b7555190dd5355264910f6708961d
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-12-07 18:50:07 +0000

    clean up code

--- tools/tellurium-ide/source/chrome/content/recorder.js
+++ tools/tellurium-ide/source/chrome/content/recorder.js
@@ -508,45 +508,6 @@ Recorder.prototype.recordCommand = function(name, element, value, valueType){
     }
 };
 
-/*
-Recorder.prototype.recordCommand = function(name, element, value, valueType){
-    logger.debug("Recording command (name: " + name + ", element: " + element + ", value: " + value + ", type: " + valueType + ")");
-    if (element != null && element != undefined) {
-        if (this.first) {
-            this.workspace.addCommand("open", null, element.ownerDocument.location.href, ValueType.STRING);
-            var ocmd = new TestCmd("open", null, element.ownerDocument.location.href);
-            this.recordCommandList.push(ocmd);
-            this.first = false;
-        }
-        var uid = this.recordDomNode(element);
-        var count = teJQuery(element).data("count");
-        if(count == undefined){
-            logger.warn("Element count is undefined for command " + name);
-            teJQuery(element).data("count", "1");
-        }else{
-            teJQuery(element).data("count", count + 1);
-        }
-        var result = this.workspace.addCommand(name, uid, value, valueType);
-        if (result) {
-            var cmd = new TestCmd(name, uid, value);
-            this.recordCommandList.push(cmd);
-            this.cmdListView.setTestCommands(this.recordCommandList);
-            this.cmdListView.rowInserted();
-            this.updateWindowUrl(element);
-            this.lastWindow = element.ownerDocument.location.href;
-        }
-    } else {
-        var result = this.workspace.addCommand(name, null, value, valueType);
-        if (result) {
-            var cmd = new TestCmd(name, null, value);
-            this.recordCommandList.push(cmd);
-            this.cmdListView.setTestCommands(this.recordCommandList);
-            this.cmdListView.rowInserted();
-        }
-    }
-};
-*/
-
 Recorder.prototype.getTextReg = function(option) {
     var label = option.text.replace(/^ *(.*?) *$/, "$1");
     if (label.match(/\xA0/)) { // if the text contains &nbsp;
