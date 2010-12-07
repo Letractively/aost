8ab0fe9560d13680638b46434a2f1047a2b1aac1
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-12-07 21:45:30 +0000

    add more pref.js files

---
+++ tools/tellurium-ide/source/defaults/preferences/winpref.js
@@ -0,0 +1,5 @@
+pref("extensions.teide.exportdirectory", "c:\\");
+pref("extensions.teide.jslog", "true");
+pref("extensions.teide.logwrap", "true");
+pref("extensions.teide.grouplocating", "true");
+pref("extensions.teide.extranodes", "true");
