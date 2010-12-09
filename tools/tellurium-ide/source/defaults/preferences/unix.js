2e540ffcc5aaddf755d478ace4e625f84f650b1d
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2010-12-09 03:32:21 +0000

    update Tellurium IDE version

--- tools/tellurium-ide/source/defaults/preferences/unix.js
+++
@@ -1,5 +0,0 @@
-pref("extensions.teide.exportdirectory", "/tmp");
-pref("extensions.teide.jslog", "true");
-pref("extensions.teide.logwrap", "true");
-pref("extensions.teide.grouplocating", "true");
-pref("extensions.teide.extranodes", "true");
--- tools/tellurium-ide/source/defaults/preferences/winpref.js
+++
@@ -1,5 +0,0 @@
-pref("extensions.teide.exportdirectory", "c:\\");
-pref("extensions.teide.jslog", "true");
-pref("extensions.teide.logwrap", "true");
-pref("extensions.teide.grouplocating", "true");
-pref("extensions.teide.extranodes", "true");
