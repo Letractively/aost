ff73b267c26b802adc8d6f7911ea063c4455ca7b
John.Jian.Fang <John.Jian.Fang@0ac36b16-8950-0410-81ec-99a1d3d0ea3c>
2011-02-21 04:11:18 +0000

    update display module

--- core/src/test/groovy/org/telluriumsource/module/DisplayModule.groovy
+++ core/src/test/groovy/org/telluriumsource/module/DisplayModule.groovy
@@ -16,6 +16,36 @@ class DisplayModule extends DslContext {
         ui.Window(uid: "testWindow", name: "TestWindow") {
 
         }
+
+        ui.Container(uid: "successMsg", clocator: [id: "successMsg"])
+        ui.Container(uid: "errorMsg", clocator: [id: "errorMsg"])
+        ui.Container(uid: "name", clocator: [tag: "body", id: "add_new_discussion"]) {
+            Container(uid: "newArticle", clocator: [tag: "div", class: "add_new_article"]) {
+                Container(uid: "head", cloator: [class: "head"]) {
+                    Container(uid: "discussionEntryModuleTitle", clocator: [tag: "h3"])
+                }
+                Container(uid: "body", clocator: [class: "body"]) {
+                    Container(uid: "discussionTitle", clocator: [tag: "li", class: "input text"]) {
+                        Container(uid: "titleLabel", clocator: [tag: "label", for: "cmsgData[topic]"]);
+                        InputBox(uid: "newDiscussionTitle", clocator: [name: "cmsgData[topic]"]);
+                        Container(uid: "errorMessage", clocator: [class: "errorInput"])
+                    }
+                    Container(uid: "category", clocator: [tag: "li", class: "input select"]) {
+                        Container(uid: "categoryLabel", clocator: [tag: "label", for: "cmsgData[category]"])
+                        Selector(uid: "discussionCategory", clocator: [name: "cmsgData[category]"]);
+                        Container(uid: "errorMessage", clocator: [class: "errorInput"])
+                    }
+                    Container(uid: "discussionTextArea", clocator: [tag: "li", class: "input textarea"]) {
+                        Container(uid: "bodyLabel", clocator: [tag: "label", for: "cmsgData[description]"]);
+                        InputBox(uid: "bodyFrame", clocator: [class: "mceContentBody ", id: "tinymce"]);
+                        Container(uid: "errorMessage", clocator: [class: "errorInput"])
+                        Container(uid: "charactersRemaining", clocator: [id: "inputCount"])
+                    }
+
+                }
+            }
+        }
+
     }
 
     public openPopupWindow() {
